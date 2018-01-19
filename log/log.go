7305c460c169a501ec2a21db59025353890c7cf6
xizhao <kevin@fossa.io>
2018-01-19 15:47:20 -0800

    feat(log): add logging config

--- log/log.go
+++ log/log.go
@@ -3,3 +3,7 @@ package log
 import logging "github.com/op/go-logging"
 
 var Log = logging.MustGetLogger("main")
+
+var Format = logging.MustStringFormatter(
+	`%{color}%{level}%{color:reset} %{message}`,
+)
