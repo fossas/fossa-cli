99cf015e9cfc09f2d83bbf1a845090556f6fd680
Leo Zhang <leo@fossa.io>
2018-03-19 19:21:45 -0700

    test(fixtures): Use shell script instead of submodules for fixtures to avoid slow go get

---
+++ test/fixtures/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Android projects
+git clone --depth=1 https://github.com/google/iosched android
+
+# Gradle projects
+git clone --depth=1 https://github.com/ReactiveX/RxJava gradle
+
+# Maven projects
+# clojure/clojure
+# sendgrid/sendgrid-java
+# google/truth
+# google/auto
+git clone --depth=1 https://github.com/apache/hadoop maven
+
+# Scala projects
+# apache/spark
+git clone --depth=1 https://github.com/twitter/finagle sbt
+
+# Node projects
+## NPM
+## Yarn
+## Bower
+
+# Ruby projects
+
+# Python projects
+
+# Go projects
+
+# PHP projects
