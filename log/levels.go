a3489715802b57143c50e2e20a07ea087a0552ce
Leo Zhang <leo@fossa.io>
2018-05-10 17:10:05 -0700

    refactor(log): Add idiomatic logging package

---
+++ log/levels.go
@@ -0,0 +1,53 @@
+package log
+
+import (
+	"fmt"
+
+	logging "github.com/op/go-logging"
+)
+
+var logger = logging.MustGetLogger("fossa-cli")
+
+// Debug outputs a literal debug message. Debug messages are useful for tracing
+// execution and diagnosing unintended error cases.
+func Debug(args ...interface{}) {
+	Debugf("%s", fmt.Sprint(args...))
+}
+
+// Debugf outputs a formatted debug message.
+func Debugf(format string, args ...interface{}) {
+	logger.Debugf(format, args...)
+}
+
+// Notice outputs a literal notice message. Notices are non-error events that
+// the user should be informed of.
+func Notice(args ...interface{}) {
+	Noticef("%s", fmt.Sprint(args...))
+}
+
+// Noticef outputs a formatted notice message.
+func Noticef(format string, args ...interface{}) {
+	logger.Noticef(format, args...)
+}
+
+// Warning outputs a literal warning message. Warnings are non-fatal error
+// events.
+func Warning(args ...interface{}) {
+	Warningf("%s", fmt.Sprint(args...))
+}
+
+// Warningf outputs a formatted warning message.
+func Warningf(format string, args ...interface{}) {
+	logger.Warningf(format, args...)
+}
+
+// Fatal outputs a literal error message. Fatal errors are non-recoverable, and
+// cause an `os.Exit(1)`.
+func Fatal(args ...interface{}) {
+	Fatalf("%s", fmt.Sprint(args...))
+}
+
+// Fatalf outputs a formatted error message.
+func Fatalf(format string, args ...interface{}) {
+	logger.Fatalf(format, args...)
+}
---
+++ log/log.go
@@ -0,0 +1,44 @@
+package log
+
+import (
+	"fmt"
+	"os"
+
+	logging "github.com/op/go-logging"
+)
+
+var useSpinner bool
+
+// Initialize sets up logging modes.
+func Initialize(interactive, debug bool) {
+	// If `interactive`, then use ANSI codes (spinner + colors)
+	useSpinner = interactive
+	var colorOn, colorOff string
+	if interactive {
+		colorOn = "%{color}"
+		colorOff = "%{color:reset}"
+	}
+
+	// If `debug`, then log in debug format and at debug level.
+	formatter := logging.MustStringFormatter(colorOn + "%{level}" + colorOff + " %{message}")
+	if debug {
+		formatter = logging.MustStringFormatter(colorOn + "%{time} %{level} %{shortpkg}/%{shortfile}/%{shortfunc}" + colorOff + " %{message}")
+	}
+	stderrBackend := logging.AddModuleLevel(logging.NewBackendFormatter(logging.NewLogBackend(os.Stderr, "", 0), formatter))
+	if debug {
+		stderrBackend.SetLevel(logging.DEBUG, "")
+	} else {
+		stderrBackend.SetLevel(logging.WARNING, "")
+	}
+	logging.SetBackend(stderrBackend)
+}
+
+// Print outputs a literal message to STDOUT.
+func Print(args ...interface{}) {
+	fmt.Print(args...)
+}
+
+// Printf outputs a formatted message to STDOUT.
+func Printf(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
+}
