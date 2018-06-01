b908880c513f936994e33648f4493c129fec9b3f
xizhao <kevin@fossa.io>
2018-06-01 15:38:02 -0700

    feat(module): define hashes type

--- module/dependency.go
+++ module/dependency.go
@@ -3,6 +3,6 @@ package module
 // Dependency represents a code library brought in by running a Build
 type Dependency struct {
 	Locator
-
+	Hashes
 	Via []ImportPath
 }
---
+++ module/hashes.go
@@ -0,0 +1,8 @@
+package module
+
+// Hashes represents checksum coordinates of a code library brought in by running a Build
+type Hashes struct {
+	SHA1   string
+	SHA256 string
+	MD5    string
+}
