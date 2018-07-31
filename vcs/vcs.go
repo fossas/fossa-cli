539008a94053971e10782c88e8b93391e1926635
Andrew Delikat <andrewdelikat@gmail.com>
2018-07-31 12:10:37 -0700

    Enable config to find VCS settings outside of current working dir

--- vcs/vcs.go
+++ vcs/vcs.go
@@ -2,7 +2,14 @@
 // systems.
 package vcs
 
-import "github.com/fossas/fossa-cli/errutil"
+import (
+	"errors"
+	"path/filepath"
+
+	"github.com/fossas/fossa-cli/errutil"
+	"github.com/fossas/fossa-cli/files"
+	"github.com/fossas/fossa-cli/monad"
+)
 
 type Type int
 
@@ -14,8 +21,70 @@ const (
 	Bazaar
 )
 
-// GetRepository returns the location of the repository containing dirname, or
+// Errors that occur when finding VCS repositories.
+var (
+	ErrNoVCSInDir   = errors.New("could not find VCS repository in directory")
+	ErrNoNearestVCS = errors.New("could not find nearest VCS repository in directory")
+)
+
+// VCSIn returns the type of VCS repository rooted at a directory, or
+// ErrNoVCSInDir if none is found.
+func VCSIn(dirname string) (string, error) {
+	either := monad.EitherStr{}
+	result := either.
+		Bind(files.FindFolder("git", filepath.Join(dirname, ".git"))).
+		Bind(files.FindFolder("svn", filepath.Join(dirname, ".svn"))).
+		Bind(files.FindFolder("hg", filepath.Join(dirname, ".hg"))).
+		Bind(files.FindFolder("bzr", filepath.Join(dirname, ".bzr")))
+	if result.Err != nil {
+		return "", result.Err
+	}
+	if result.Result == "" {
+		return "", ErrNoVCSInDir
+	}
+	return result.Result, nil
+}
+
+// NearestVCS returns the type and directory of the nearest VCS repository
+// containing the directory, or ErrNoNearestVCS if none is found.
+func NearestVCS(dirname string) (vcsName string, vcsDir string, err error) {
+	vcsDir, err = files.WalkUp(dirname, func(d string) error {
+		tool, err := VCSIn(d)
+		if err == ErrNoVCSInDir {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		vcsName = tool
+		return files.ErrStopWalk
+	})
+	if err == files.ErrDirNotFound {
+		return "", "", ErrNoNearestVCS
+	}
+	return vcsName, vcsDir, err
+}
+
+// GetRepository returns the location of the repository containing dirname,
 // errutil.ErrRepositoryNotFound on error.
 func GetRepository(dirname string) (string, error) {
-	return "", errutil.ErrNotImplemented
+	vcsName, vcsDir, err := NearestVCS(dirname)
+	if err == ErrNoNearestVCS {
+		return "", errutil.ErrRepositoryNotFound
+	}
+	if err != nil {
+		return "", err
+	}
+	switch vcsName {
+	case "git":
+		return vcsDir, nil
+	case "svn":
+		return "", errutil.ErrNotImplemented
+	case "hg":
+		return "", errutil.ErrNotImplemented
+	case "bzr":
+		return "", errutil.ErrNotImplemented
+	default:
+		return "", errutil.ErrNotImplemented
+	}
 }
