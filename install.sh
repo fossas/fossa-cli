21299010b20515f517041691740036ab0db71386
Leo Zhang <leo@fossa.io>
2018-08-14 15:03:36 -0700

    release(v0.7.3-1): Release version v0.7.3-1

--- install.sh
+++ install.sh
@@ -109,7 +109,7 @@ function install {
   # DO NOT REMOVE THE FOLLOWING LINES. THEY ARE AUTOMATICALLY POPULATED DURING
   # THE RELEASE PROCESS.
   # ============= DO NOT EDIT ==================
-  RELEASE='v0.7.3'
+  RELEASE='v0.7.3-1'
   # ============= =========== ==================
 
   VERSION=${RELEASE#v}
