e290faf67b07c9bbe1d41f3b3adc9ddc24375099
Alex Nuccio <anuccio1@gmail.com>
2018-08-31 00:36:10 -0700

    Added Jira project key and Project URL flags (#237)
    Added Jira Project Key and Project URL flags to fossa analyze

--- cmd/fossa/cmd/analyze/analyze.go
+++ cmd/fossa/cmd/analyze/analyze.go
@@ -134,7 +134,19 @@ func Do(modules []module.Module) (analyzed []module.Module, err error) {
 
 func uploadAnalysis(normalized []fossa.SourceUnit) error {
 	display.InProgress("Uploading analysis...")
-	locator, err := fossa.Upload(config.Fetcher(), config.Project(), config.Revision(), config.Title(), config.Branch(), normalized)
+	locator, err := fossa.Upload(
+		config.Title(),
+		fossa.Locator{
+			Fetcher:  config.Fetcher(),
+			Project:  config.Project(),
+			Revision: config.Revision(),
+		},
+		fossa.UploadOptions{
+			Branch:         config.Branch(),
+			ProjectURL:     config.ProjectURL(),
+			JIRAProjectKey: config.JIRAProjectKey(),
+		},
+		normalized)
 	display.ClearProgress()
 	if err != nil {
 		log.Fatalf("Error during upload: %s", err.Error())
--- config/keys.go
+++ config/keys.go
@@ -98,6 +98,14 @@ func Branch() string {
 	return TryStrings(MockBranch, StringFlag(flags.Branch), file.Branch(), inferred, "master")
 }
 
+func ProjectURL() string {
+	return TryStrings(StringFlag(flags.ProjectURL), file.ProjectURL(), "")
+}
+
+func JIRAProjectKey() string {
+	return TryStrings(StringFlag(flags.JIRAProjectKey), file.JIRAProjectKey(), "")
+}
+
 /**** Analysis configuration keys ****/
 
 func Options() (map[string]interface{}, error) {
