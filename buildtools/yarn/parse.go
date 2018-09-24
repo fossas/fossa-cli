0e6bf19e9c2372d0a6cb546766e57bafe9e60739
Matt Schwartz <mattschwartz310@gmail.com>
2018-09-24 14:41:22 -0400

    initial pass as setting up yarn lockfile parsing

--- buildtools/yarn/parse.go
+++ buildtools/yarn/parse.go
@@ -1,1 +1,27 @@
 package yarn
+
+import (
+	"path/filepath"
+
+	"github.com/fossas/fossa-cli/files"
+)
+
+type lockfileEntry struct {
+	Version      string
+	Dependencies map[string]string
+}
+
+type YarnLockfile []lockfileEntry
+
+func FromLockfile(pathElems ...string) (YarnLockfile, error) {
+	var lockfile YarnLockfile
+
+	filePath := filepath.Join(pathElems...)
+
+	err := files.ReadYAML(&lockfile, filePath)
+	if err != nil {
+		return YarnLockfile{}, err
+	}
+
+	return lockfile, nil
+}
--- buildtools/yarn/parse_test.go
+++ buildtools/yarn/parse_test.go
@@ -1,1 +1,15 @@
 package yarn_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/fossas/fossa-cli/buildtools/yarn"
+)
+
+func TestParse(t *testing.T) {
+	lockfile, err := yarn.FromLockfile("testdata", "yarn.lock")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, lockfile)
+}
