2687e79d31065c92a9b3476ffbcd3132336f5219
Matt Schwartz <mattschwartz310@gmail.com>
2018-09-25 10:24:31 -0700

    create full test for using lockfile

--- buildtools/yarn/parse_test.go
+++ buildtools/yarn/parse_test.go
@@ -25,19 +25,40 @@ var chaiDirectDep = pkg.Import{
 }
 
 /*
-├─ chai@4.1.2
-│  ├─ assertion-error@^1.0.1
-│  ├─ check-error@^1.0.1
-│  ├─ deep-eql@^3.0.0
-│  ├─ get-func-name@^2.0.0
-│  ├─ pathval@^1.0.0
-│  └─ type-detect@^4.0.0
+└─┬ chai@4.1.2
+  ├── assertion-error@1.1.0
+  ├── check-error@1.0.2
+  ├─┬ deep-eql@3.0.1
+  │ └── type-detect@4.0.8 deduped
+  ├── get-func-name@2.0.0
+  ├── pathval@1.1.0
+  └── type-detect@4.0.8
 */
 func TestSimpleLockfile(t *testing.T) {
-	pkg, err := yarn.FromLockfile("testdata", "yarn.lock")
+	deps, err := yarn.FromLockfile("testdata", "yarn.lock")
 	assert.NoError(t, err)
 
-	assert.Len(t, pkg.Direct, 1)
-	assert.Len(t, pkg.Transitive, 7)
-	chaiDep := pkg.Imports[chaiDirectDep]
+	assert.Len(t, deps.Direct, 1)
+	assert.Len(t, deps.Transitive, 7)
+
+	assert.Equal(t, "chai", deps.Direct[0].Target)
+	assert.Equal(t, "4.1.2", deps.Direct[0].Resolved.Revision)
+
+	AssertDeps(t, deps.Transitive, "assertion-error", "1.1.0")
+	AssertDeps(t, deps.Transitive, "check-error", "1.1.2")
+	AssertDeps(t, deps.Transitive, "deep-eql", "1.1.1")
+	AssertDeps(t, deps.Transitive, "get-func-name", "2.0.0")
+	AssertDeps(t, deps.Transitive, "pathval", "1.1.0")
+	AssertDeps(t, deps.Transitive, "type-detect", "4.0.8")
+	AssertDeps(t, deps.Transitive, "chai", "4.1.2")
+}
+
+func AssertDeps(t *testing.T, transitiveDeps map[pkg.ID]pkg.Package, name, revision string) {
+	for pkgID, _ := range transitiveDeps {
+		if pkgID.Name == name && pkgID.Revision == revision {
+			return
+		}
+	}
+
+	assert.Fail(t, "missing "+name+"@"+revision)
 }
