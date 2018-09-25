5cef24591394efaa1cfa86300e37e6557e5e48fa
Matt Schwartz <mattschwartz310@gmail.com>
2018-09-25 10:11:28 -0700

    fix import ordering

--- cmd/fossa/main_test.go
+++ cmd/fossa/main_test.go
@@ -3,9 +3,10 @@ package main_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	main "github.com/fossas/fossa-cli/cmd/fossa"
 	"github.com/fossas/fossa-cli/cmd/fossa/flags"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestMainProvidesDebugFlag(t *testing.T) {
