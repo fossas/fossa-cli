5445e818eaa59cf5170731e338bd47902cc477a1
Matt Schwartz <mattschwartz310@gmail.com>
2018-09-25 11:11:47 -0700

    build direct deps properly

--- buildtools/yarn/parse.go
+++ buildtools/yarn/parse.go
@@ -1,12 +1,14 @@
 package yarn
 
 import (
+	"errors"
 	"path/filepath"
 	"regexp"
 
 	"github.com/fossas/fossa-cli/buildtools/npm"
 	"github.com/fossas/fossa-cli/files"
 	"github.com/fossas/fossa-cli/graph"
+	"github.com/fossas/fossa-cli/pkg"
 	"gopkg.in/yaml.v2"
 )
 
@@ -20,18 +22,73 @@ type YarnLockfile map[string]lockfileEntry
 // FromLockfile builds a dependency graph based on the provided path to yarn.lock
 func FromLockfile(pathElems ...string) (graph.Deps, error) {
 	filePath := filepath.Join(pathElems...)
-	yarnLockfileExists, err := files.ExistsFolder(filePath)
+	yarnLockfileExists, err := files.Exists(filePath)
 	if err != nil {
 		return graph.Deps{}, err
 	}
-	npm.FromManifest()
+	if !yarnLockfileExists {
+		return graph.Deps{}, errors.New(filePath + " does not exist")
+	}
+
+	// To know which deps are direct, we need the manifest def
+	manifestPath := filepath.Join(filepath.Dir(filePath), "package.json")
+	// manifest, err := npm.FromManifest(manifestPath)
+	// if err != nil {
+	// 	return graph.Deps{}, nil
+	// }
+	// println(manifest.Dependencies)
+
 	lockfile, err := readLockfile(filePath)
 	if err != nil {
 		return graph.Deps{}, nil
 	}
 
-	return graph.Deps{}, nil
+	for name, entry := range lockfile {
+		println(name + "->" + entry.Version)
+		for depName, depVersion := range entry.Dependencies {
+			println("\t" + depName + "@" + depVersion)
+		}
+	}
+
+	directDeps, err := directDepsFromManifest(manifestPath)
+	if err != nil {
+		return graph.Deps{}, nil
+	}
+
+	return graph.Deps{
+		Direct:     directDeps,
+		Transitive: make(map[pkg.ID]pkg.Package),
+	}, nil
+
+	// return depGraph(rootPkg.Imports, lockfile)
 }
+
+func directDepsFromManifest(manifestPath string) (pkg.Imports, error) {
+	manifest, err := npm.FromManifest(manifestPath)
+	if err != nil {
+		return pkg.Imports{}, nil
+	}
+
+	directDeps := make(pkg.Imports, len(manifest.Dependencies))
+
+	i := 0
+	for name, revision := range manifest.Dependencies {
+		directDeps[i] = pkg.Import{
+			Target: name,
+			Resolved: pkg.ID{
+				Name:     name,
+				Revision: revision,
+				Type:     pkg.NodeJS,
+			},
+		}
+		i++
+	}
+
+	return directDeps, nil
+}
+
+// func depGraph(directDeps pkg.Imports, lockfile YarnLockfile) (graph.Deps, error) {
+// }
 
 func readLockfile(pathElems ...string) (YarnLockfile, error) {
 	var lockfile YarnLockfile
