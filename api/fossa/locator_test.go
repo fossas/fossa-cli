f7bc7ea16e3b52c6f455bf5be5e2490668e11c83
Zach <zcjlavallee@gmail.com>
2018-09-25 19:20:29 -0700

    Fix/test upload (#289)
    * correct url returned on analyze
    
    * split normalization and string modification
    
    * locator tests added for git fetcher
    
    * test suite for custom fetchers
    
    * privatize cacheID

--- api/fossa/locator_test.go
+++ api/fossa/locator_test.go
@@ -37,13 +37,129 @@ func TestLocatorFetcher(t *testing.T) {
 }
 
 func TestStringGit(t *testing.T) {
-	custom := fossa.Locator{
+	ssh := fossa.Locator{
 		Fetcher:  "git",
 		Project:  "git@github.com:fossas/fossa-cli.git",
 		Revision: "SHAVALUE",
 	}
 
-	stringified := custom.String()
+	http := fossa.Locator{
+		Fetcher:  "git",
+		Project:  "http://github.com/fossas/fossa-cli.git",
+		Revision: "SHAVALUE",
+	}
+
+	sshActual := ssh.String()
+	httpActual := http.String()
 	expected := "git+github.com/fossas/fossa-cli$SHAVALUE"
+	assert.Equal(t, sshActual, expected)
+	assert.Equal(t, httpActual, expected)
+}
+
+func TestStringCustom(t *testing.T) {
+	fossa.MockOrgID = "3000"
+	custom := fossa.Locator{
+		Fetcher:  "custom",
+		Project:  "fossa-cli",
+		Revision: "SHAVALUE",
+	}
+
+	customActual := custom.String()
+	expected := "custom+fossa-cli$SHAVALUE"
+	assert.Equal(t, customActual, expected)
+}
+
+func TestStringCustomGitHTTP(t *testing.T) {
+	http := fossa.Locator{
+		Fetcher:  "custom",
+		Project:  "http://github.com/fossa/fossa-cli.git",
+		Revision: "SHAVALUE",
+	}
+
+	stringified := http.String()
+	expected := "custom+http://github.com/fossa/fossa-cli.git$SHAVALUE"
+	assert.Equal(t, stringified, expected)
+}
+
+func TestStringCustomGitSSH(t *testing.T) {
+	ssh := fossa.Locator{
+		Fetcher:  "custom",
+		Project:  "git@github.com:fossa/fossa-cli.git",
+		Revision: "SHAVALUE",
+	}
+
+	stringified := ssh.String()
+	expected := "custom+git@github.com:fossa/fossa-cli.git$SHAVALUE"
+	assert.Equal(t, stringified, expected)
+}
+
+func TestOrgStringGit(t *testing.T) {
+	ssh := fossa.Locator{
+		Fetcher:  "git",
+		Project:  "git@github.com:fossas/fossa-cli.git",
+		Revision: "SHAVALUE",
+	}
+
+	http := fossa.Locator{
+		Fetcher:  "git",
+		Project:  "http://github.com/fossas/fossa-cli.git",
+		Revision: "SHAVALUE",
+	}
+
+	sshActual := ssh.OrgString()
+	httpActual := http.OrgString()
+	expected := "git+github.com/fossas/fossa-cli$SHAVALUE"
+	assert.Equal(t, sshActual, expected)
+	assert.Equal(t, httpActual, expected)
+}
+
+func TestOrgStringCustom(t *testing.T) {
+	fossa.MockOrgID = "3000"
+	git := fossa.Locator{
+		Fetcher:  "custom",
+		Project:  "fossa-cli",
+		Revision: "SHAVALUE",
+	}
+
+	stringified := git.OrgString()
+	expected := "custom+3000/fossa-cli$SHAVALUE"
+	assert.Equal(t, stringified, expected)
+}
+
+func TestOrgStringCustomGitHTTP(t *testing.T) {
+	fossa.MockOrgID = "3000"
+	http := fossa.Locator{
+		Fetcher:  "custom",
+		Project:  "http://github.com/fossa/fossa-cli.git",
+		Revision: "SHAVALUE",
+	}
+
+	stringified := http.OrgString()
+	expected := "custom+3000/github.com/fossa/fossa-cli$SHAVALUE"
+	assert.Equal(t, stringified, expected)
+}
+
+func TestOrgStringCustomGitSSH(t *testing.T) {
+	fossa.MockOrgID = "3000"
+	ssh := fossa.Locator{
+		Fetcher:  "custom",
+		Project:  "git@github.com:fossa/fossa-cli.git",
+		Revision: "SHAVALUE",
+	}
+
+	stringified := ssh.OrgString()
+	expected := "custom+3000/git@github.com:fossa/fossa-cli.git$SHAVALUE"
+	assert.Equal(t, stringified, expected)
+}
+
+func TestURL(t *testing.T) {
+	git := fossa.Locator{
+		Fetcher:  "git",
+		Project:  "git@github.com:fossas/fossa-cli.git",
+		Revision: "SHAVALUE",
+	}
+
+	stringified := git.URL()
+	expected := "https://app.fossa.io/projects/git+git@github.com:fossas%2Ffossa-cli.git/refs/branch/master/SHAVALUE"
 	assert.Equal(t, stringified, expected)
 }
