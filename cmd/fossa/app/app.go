dff5f88fa580073139ad3d6c1bf79767853a2eb8
Matt Schwartz <mattschwartz310@gmail.com>
2018-09-26 17:31:25 -0700

    move main entry point somewhere that is testable

---
+++ cmd/fossa/app/app.go
@@ -0,0 +1,66 @@
+package app
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/urfave/cli"
+
+	"github.com/fossas/fossa-cli/cmd/fossa/cmd/analyze"
+	"github.com/fossas/fossa-cli/cmd/fossa/cmd/build"
+	initc "github.com/fossas/fossa-cli/cmd/fossa/cmd/init"
+	"github.com/fossas/fossa-cli/cmd/fossa/cmd/report"
+	"github.com/fossas/fossa-cli/cmd/fossa/cmd/test"
+	"github.com/fossas/fossa-cli/cmd/fossa/cmd/update"
+	"github.com/fossas/fossa-cli/cmd/fossa/cmd/upload"
+	"github.com/fossas/fossa-cli/cmd/fossa/flags"
+	"github.com/fossas/fossa-cli/cmd/fossa/setup"
+	"github.com/fossas/fossa-cli/cmd/fossa/version"
+	"github.com/fossas/fossa-cli/config"
+)
+
+var App = cli.App{
+	Name:                 "fossa-cli",
+	Usage:                "Fast, portable and reliable dependency analysis (https://github.com/fossas/fossa-cli/)",
+	Version:              version.String(),
+	Action:               Run,
+	EnableBashCompletion: true,
+	Flags: flags.Combine(
+		initc.Cmd.Flags,
+		analyze.Cmd.Flags,
+		flags.WithGlobalFlags(nil),
+	),
+	Commands: []cli.Command{
+		initc.Cmd,
+		build.Cmd,
+		analyze.Cmd,
+		upload.Cmd,
+		report.Cmd,
+		test.Cmd,
+		update.Cmd,
+	},
+}
+
+func Run(ctx *cli.Context) error {
+	err := setup.SetContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	if config.APIKey() == "" && !ctx.Bool(analyze.ShowOutput) {
+		fmt.Printf("Incorrect Usage. FOSSA_API_KEY must be set as an environment variable or provided in .fossa.yml\n\n")
+		log.Fatalf("No API KEY provided")
+	}
+
+	err = initc.Run(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = analyze.Run(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- cmd/fossa/main.go
+++ cmd/fossa/main.go
@@ -1,51 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/urfave/cli"
+	"github.com/fossas/fossa-cli/cmd/fossa/app"
 
 	"github.com/apex/log"
-	"github.com/fossas/fossa-cli/config"
-
-	"github.com/fossas/fossa-cli/cmd/fossa/flags"
-	"github.com/fossas/fossa-cli/cmd/fossa/setup"
-	"github.com/fossas/fossa-cli/cmd/fossa/version"
-
-	"github.com/fossas/fossa-cli/cmd/fossa/cmd/analyze"
-	"github.com/fossas/fossa-cli/cmd/fossa/cmd/build"
-	initc "github.com/fossas/fossa-cli/cmd/fossa/cmd/init"
-	"github.com/fossas/fossa-cli/cmd/fossa/cmd/report"
-	"github.com/fossas/fossa-cli/cmd/fossa/cmd/test"
-	"github.com/fossas/fossa-cli/cmd/fossa/cmd/update"
-	"github.com/fossas/fossa-cli/cmd/fossa/cmd/upload"
+	"github.com/urfave/cli"
 )
 
-var App = cli.App{
-	Name:                 "fossa-cli",
-	Usage:                "Fast, portable and reliable dependency analysis (https://github.com/fossas/fossa-cli/)",
-	Version:              version.String(),
-	Action:               Run,
-	EnableBashCompletion: true,
-	Flags: flags.Combine(
-		initc.Cmd.Flags,
-		analyze.Cmd.Flags,
-		flags.WithGlobalFlags(nil),
-	),
-	Commands: []cli.Command{
-		initc.Cmd,
-		build.Cmd,
-		analyze.Cmd,
-		upload.Cmd,
-		report.Cmd,
-		test.Cmd,
-		update.Cmd,
-	},
-}
-
 func main() {
-	err := App.Run(os.Args)
+	err := app.App.Run(os.Args)
 	if err != nil {
 		switch e := err.(type) {
 		case *cli.ExitError:
@@ -56,28 +21,4 @@ func main() {
 			os.Exit(1)
 		}
 	}
-}
-
-func Run(ctx *cli.Context) error {
-	err := setup.SetContext(ctx)
-	if err != nil {
-		return err
-	}
-
-	if config.APIKey() == "" && !ctx.Bool(analyze.ShowOutput) {
-		fmt.Printf("Incorrect Usage. FOSSA_API_KEY must be set as an environment variable or provided in .fossa.yml\n\n")
-		log.Fatalf("No API KEY provided")
-	}
-
-	err = initc.Run(ctx)
-	if err != nil {
-		return err
-	}
-
-	err = analyze.Run(ctx)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
