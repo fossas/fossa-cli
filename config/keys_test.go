8e7b1b592d6183d90a768f1f3a3ecb6061a0450c
Zachary LaVallee <zcjlavallee@gmail.com>
2018-10-18 12:58:13 -0700

    fix personal nits

--- config/keys_test.go
+++ config/keys_test.go
@@ -12,19 +12,19 @@ import (
 )
 
 func TestModulesCommandLineOptions(t *testing.T) {
-	// Set the expected flags
+	// Set the expected flags.
 	flagSet := flag.NewFlagSet("test", 0)
 	flags.ConfigF.Apply(flagSet)
 	flags.OptionF.Apply(flagSet)
 
-	// Set the flag values
+	// Set the flag values.
 	ctx := cli.NewContext(cli.NewApp(), flagSet, nil)
 	err := ctx.Set(flags.Config, "testdata/test.yml")
 	assert.NoError(t, err)
 	err = ctx.Set(flags.Option, "allow-unresolved:true")
 	assert.NoError(t, err)
 
-	//Finalize the context
+	// Finalize the context.
 	err = config.SetContext(ctx)
 	assert.NoError(t, err)
 
@@ -40,7 +40,7 @@ func TestModulesCommandLineOptions(t *testing.T) {
 	assert.Equal(t, true, modules[1].Options["allow-unresolved"])
 	assert.Equal(t, "linux", modules[1].Options["os"])
 
-	// Test that command line options are preferred when they conflict.
+	// Test that command line options are preferred when they conflict config options.
 	assert.Equal(t, 1, len(modules[2].Options))
 	assert.Equal(t, true, modules[2].Options["allow-unresolved"])
 }
