7d01a25c2481feca4f5db2f2d2022b5d8cbb0896
Leo Zhang <leo@fossa.io>
2018-10-18 12:08:53 -0700

    test: Restore CWD after each test

--- config/config_test.go
+++ config/config_test.go
@@ -9,12 +9,24 @@ import (
 	"github.com/urfave/cli"
 
 	"github.com/fossas/fossa-cli/config"
+	"github.com/fossas/fossa-cli/vcs"
 )
 
 func TestInitWorksWithoutGitRepository(t *testing.T) {
-	err := os.Chdir("/")
+	// Change to a directory without Git repository.
+	cwd, err := os.Getwd()
 	assert.NoError(t, err)
+	err = os.Chdir("/")
+	assert.NoError(t, err)
+	defer func() {
+		os.Chdir(cwd)
+	}()
 
+	// Check that there actually is no Git repository.
+	_, _, err = vcs.Nearest(".")
+	assert.Error(t, err, vcs.ErrNoNearestVCS)
+
+	// Check that config.setContext doesn't break.
 	ctx := cli.NewContext(cli.NewApp(), flag.NewFlagSet("test", 0), nil)
 	err = config.SetContext(ctx)
 	assert.NoError(t, err)
