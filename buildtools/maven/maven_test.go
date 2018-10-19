f414826880e4dbc4ed2f2479bc064aa472238d78
Zachary LaVallee <zcjlavallee@gmail.com>
2018-10-18 17:53:51 -0700

    check for file formats

--- buildtools/maven/maven_test.go
+++ buildtools/maven/maven_test.go
@@ -2,6 +2,7 @@ package maven_test
 
 import (
 	"io/ioutil"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,7 +11,12 @@ import (
 )
 
 func TestParseDependencyTreeDOS(t *testing.T) {
-	dat, err := ioutil.ReadFile("testdata/dos.out")
+	// Check that the file is still dos formatted.
+	dosFile := "testdata/dos.out"
+	out, _ := exec.Command("file", dosFile).Output()
+	assert.Equal(t, "testdata/dos.out: ASCII text, with CRLF line terminators\n", string(out))
+
+	dat, err := ioutil.ReadFile(dosFile)
 	assert.NoError(t, err)
 	direct, transitive, err := maven.ParseDependencyTree(string(dat))
 	assert.NoError(t, err)
@@ -19,7 +25,12 @@ func TestParseDependencyTreeDOS(t *testing.T) {
 }
 
 func TestParseDependencyTreeUnix(t *testing.T) {
-	dat, err := ioutil.ReadFile("testdata/osx.out")
+	// Check that the file is still unix formatted.
+	osxFile := "testdata/osx.out"
+	out, _ := exec.Command("file", osxFile).Output()
+	assert.Equal(t, "testdata/osx.out: ASCII text\n", string(out))
+
+	dat, err := ioutil.ReadFile(osxFile)
 	assert.NoError(t, err)
 	direct, transitive, err := maven.ParseDependencyTree(string(dat))
 	assert.NoError(t, err)
