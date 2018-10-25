6d1169f61af865ad1074e3e895587495d6e6ae12
Zachary LaVallee <zcjlavallee@gmail.com>
2018-10-25 13:26:10 -0700

    test on CI

--- buildtools/maven/maven_test.go
+++ buildtools/maven/maven_test.go
@@ -24,6 +24,20 @@ func TestParseDependencyTreeDOS(t *testing.T) {
 	assert.NotEmpty(t, direct)
 	assert.NotEmpty(t, transitive)
 }
+func TestParseDependencyTreeDOSCat(t *testing.T) {
+	// Check that the file is still dos formatted.
+	dosFile := "testdata/dos.out"
+	cat, err := exec.Command("cat", "-e", dosFile).Output()
+	assert.NoError(t, err)
+	assert.Contains(t, string(cat), "^M$")
+
+	dat, err := ioutil.ReadFile(dosFile)
+	assert.NoError(t, err)
+	direct, transitive, err := maven.ParseDependencyTree(string(dat))
+	assert.NoError(t, err)
+	assert.NotEmpty(t, direct)
+	assert.NotEmpty(t, transitive)
+}
 
 func TestParseDependencyTreeUnix(t *testing.T) {
 	// Check that the file is still unix formatted.
@@ -31,6 +45,22 @@ func TestParseDependencyTreeUnix(t *testing.T) {
 	out, err := exec.Command("file", osxFile).Output()
 	assert.NoError(t, err)
 	assert.Equal(t, "testdata/osx.out: ASCII text\n", string(out))
+
+	dat, err := ioutil.ReadFile(osxFile)
+	assert.NoError(t, err)
+	direct, transitive, err := maven.ParseDependencyTree(string(dat))
+	assert.NoError(t, err)
+	assert.NotEmpty(t, direct)
+	assert.NotEmpty(t, transitive)
+}
+
+func TestParseDependencyTreeOSXCat(t *testing.T) {
+	// Check that the file is still dos formatted.
+	osxFile := "testdata/osx.out"
+	cat, err := exec.Command("cat", "-e", osxFile).Output()
+	assert.NoError(t, err)
+	assert.NotContains(t, string(cat), "^M$")
+	assert.Contains(t, string(cat), "$")
 
 	dat, err := ioutil.ReadFile(osxFile)
 	assert.NoError(t, err)
