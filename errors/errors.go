f1e48b0227619a4cce750d2159d1b6973976f2f4
Zachary LaVallee <zcjlavallee@gmail.com>
2019-07-02 10:11:40 -0700

    change support to email

--- errors/errors.go
+++ errors/errors.go
@@ -23,7 +23,7 @@ TROUBLESHOOTING:
 Please try troubleshooting before filing a bug. If none of the suggestions work,
 you can file a bug at <https://github.com/fossas/fossa-cli/issues/new>.
 
-For additional support, ask the #cli channel at <https://slack.fossa.io>.
+For additional support send an email to support@fossa.com.
 
 CREATING AN ISSUE:
 
