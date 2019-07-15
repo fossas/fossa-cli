ede67356ebe92e410f2e05c7c01d7c1ce1d4686b
Zachary LaVallee <zcjlavallee@gmail.com>
2019-07-15 16:35:49 -0700

    messages file

---
+++ errors/messages.go
@@ -0,0 +1,13 @@
+package errors
+
+const defaultMessage = `
+
+Please try troubleshooting before filing a bug. If none of the suggestions work,
+you can file a bug at <https://github.com/fossas/fossa-cli/issues/new>.
+
+CREATING AN ISSUE:
+
+Before creating an issue, please search GitHub issues for similar problems. When
+creating the issue, please attach the debug log located at:
+
+  /tmp/fossa-cli-debug-log`
