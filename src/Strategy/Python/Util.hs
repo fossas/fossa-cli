59733095522a4137a61ce6d676198d2bc5821d1e
Connor James <connor@fossa.com>
2019-10-10 12:41:27 -0700

    Add test for requirements parser

--- src/Strategy/Python/Util.hs
+++ src/Strategy/Python/Util.hs
@@ -66,11 +66,6 @@ data Req =
   | UrlReq Text (Maybe [Text]) URI.URI (Maybe Marker) -- name extras, ...
   deriving (Eq, Ord, Show, Generic)
 
--- TODO: move this to test suite
-test :: IO ()
-test = traverse_ (parseTest requirementParser)
-  [ "A", "A.B-C_D", "aa", "name", "name<=1", "name>=3", "name>=3,<2", "name@http://foo.com", "name [fred,bar] @ http://foo.com ; python_version=='2.7'", "name[quux, strange];python_version<'2.7' and platform_version=='2'", "name; os_name=='a' or os_name=='b'", "name; os_name=='a' and os_name=='b' or os_name=='c'", "name; os_name=='a' and (os_name=='b' or os_name=='c')", "name; os_name=='a' or os_name=='b' and os_name=='c'", "name; (os_name=='a' or os_name=='b') and os_name=='c'" ]
-
 -- grammar extracted from https://www.python.org/dev/peps/pep-0508/
 requirementParser :: Parser Req
 requirementParser = specification
---
+++ test/Python/RequirementsTest.hs
@@ -0,0 +1,39 @@
+
+module Python.RequirementsTest
+  ( spec_parser
+  ) where
+
+import Prelude
+
+import Data.Foldable
+import Data.Text (Text)
+import Text.Megaparsec
+
+import Strategy.Python.Util (requirementParser)
+
+import Test.Tasty.Hspec
+
+spec_parser :: Spec
+spec_parser = do
+  describe "requirementParser" $ do
+    it "can parse the edge case examples" $ do
+      traverse_ (parseTest requirementParser) examples
+
+examples :: [Text]
+examples =
+  [ "A"
+  , "A.B-C_D"
+  , "aa"
+  , "name"
+  , "name<=1"
+  , "name>=3"
+  , "name>=3,<2"
+  , "name@http://foo.com"
+  , "name [fred,bar] @ http://foo.com ; python_version=='2.7'"
+  , "name[quux, strange];python_version<'2.7' and platform_version=='2'"
+  , "name; os_name=='a' or os_name=='b'"
+  , "name; os_name=='a' and os_name=='b' or os_name=='c'"
+  , "name; os_name=='a' and (os_name=='b' or os_name=='c')"
+  , "name; os_name=='a' or os_name=='b' and os_name=='c'"
+  , "name; (os_name=='a' or os_name=='b') and os_name=='c'"
+  ]
