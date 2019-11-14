7c7aa6f083a3ea10f5bebbec2cc9435a83b8926f
Matthew Schwartz <mattschwartz310@gmail.com>
2019-11-14 14:43:15 -0800

    [FC-1212] create integration with new report endpoint (#554)

---
+++ api/fossa/attribution_report.go
@@ -0,0 +1,60 @@
+package fossa
+
+import (
+	"fmt"
+	"net/url"
+)
+
+// AttributionDependencyLicense is the structure of a license in an attribution report
+type AttributionDependencyLicense struct {
+	Name        string
+	Attribution string
+}
+
+// AttributionReportRow is the structure of a single dependency in an attribution report
+type AttributionReportRow struct {
+	Package         string
+	Source          string
+	Version         string
+	IsGolang        bool
+	Hash            string
+	Authors         []string
+	Description     string
+	Licenses        []AttributionDependencyLicense
+	OtherLicenses   []AttributionDependencyLicense
+	ProjectURL      string
+	DependencyPaths []string
+	Notes           []string
+	DownloadURL     string
+}
+
+// AttributionReportProject is the project for which the report is being made
+type AttributionReportProject struct {
+	Name     string
+	Revision string
+}
+
+// AttributionReport is the report including the dependency and license data for a project
+type AttributionReport struct {
+	Project            AttributionReportProject
+	DirectDependencies []AttributionReportRow
+	DeepDependencies   []AttributionReportRow
+	Licenses           map[string]string
+}
+
+const reportAPI = "/api/revisions/%s/attribution/json"
+
+// GetReport gets a report for the provided locator
+func GetReport(locator Locator) (AttributionReport, error) {
+	var report AttributionReport
+	params := url.Values{}
+
+	params.Add("includeDeepDependencies", "true")
+	params.Add("includeHashAndVersionData", "true")
+	params.Add("includeDownloadUrl", "true")
+
+	url := fmt.Sprintf(reportAPI, url.PathEscape(locator.OrgString())) + "?" + params.Encode()
+	_, err := GetJSON(url, &report)
+
+	return report, err
+}
