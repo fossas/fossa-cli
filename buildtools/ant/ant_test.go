0054e4b4d6ec2653615f5c5a84af6eeae72b31d1
Connor James <connor@fossa.com>
2021-02-16 11:50:21 -0800

    Ant: use pom.xml's <parent> version if one isn't declared at the top level

--- buildtools/ant/ant_test.go
+++ buildtools/ant/ant_test.go
@@ -12,10 +12,13 @@ import (
 func TestAntDeps(t *testing.T) {
 	graph, err := ant.Graph("testdata")
 	assert.Nil(t, err)
-	assert.Len(t, graph.Direct, 1)
+	assert.Len(t, graph.Direct, 2)
 	helpers.AssertPackageImport(t, graph.Direct, "test", "1.2.3")
+	helpers.AssertPackageImport(t, graph.Direct, "io.dropwizard.metrics:metrics-core", "3.1.5")
 
-	assert.Len(t, graph.Transitive, 1)
+	assert.Len(t, graph.Transitive, 2)
 	packageOne := helpers.PackageInTransitiveGraph(graph.Transitive, "test", "1.2.3")
 	assert.Len(t, packageOne.Imports, 0)
+	packageTwo := helpers.PackageInTransitiveGraph(graph.Transitive, "io.dropwizard.metrics:metrics-core", "3.1.5")
+	assert.Len(t, packageTwo.Imports, 0)
 }
