6f8fb342e4b102b819efd3e98d943805e1fe93bf
Enrico Borba <enricozb@users.noreply.github.com>
2021-03-25 07:56:26 -1000

    Bump Basis Version (#215)

--- vendor_download.sh
+++ vendor_download.sh
@@ -41,7 +41,7 @@ esac
 TAG="latest"
 echo "Downloading asset information from latest tag for architecture '$ASSET_POSTFIX'"
 
-WIGGINS_TAG="staging-2021-03-05-492d37d"
+WIGGINS_TAG="2021-03-24-f46fe27"
 echo "Downloading wiggins binary"
 echo "Using wiggins release: $WIGGINS_TAG"
 WIGGINS_RELEASE_JSON=vendor/wiggins-release.json
