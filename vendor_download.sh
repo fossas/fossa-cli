8aa65a8492f5430d36cc56a837cf7732b97ddc74
Kit Martin <kit@fossa.com>
2021-03-30 19:06:33 -0700

    Update Wiggins to 2021-03-30-28383e8 (#216)

--- vendor_download.sh
+++ vendor_download.sh
@@ -41,7 +41,7 @@ esac
 TAG="latest"
 echo "Downloading asset information from latest tag for architecture '$ASSET_POSTFIX'"
 
-WIGGINS_TAG="2021-03-24-f46fe27"
+WIGGINS_TAG="2021-03-30-28383e8"
 echo "Downloading wiggins binary"
 echo "Using wiggins release: $WIGGINS_TAG"
 WIGGINS_RELEASE_JSON=vendor/wiggins-release.json
