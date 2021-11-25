3f3cde850dc29a1078571dc022e00c039962418c
Megh <megh@fossa.com>
2021-11-24 18:37:05 -0700

    updates revision of poetry src code

--- integration-test/Analysis/Python/PoetrySpec.hs
+++ integration-test/Analysis/Python/PoetrySpec.hs
@@ -16,7 +16,7 @@ poetry =
     Poetry.discover
     LocalEnvironment
     Nothing
-    $ FixtureArtifact "https://github.com/python-poetry/poetry/archive/refs/tags/0.12.7.tar.gz" [reldir|python/poetry/poetry/|] (Just [reldir|poetry-0.12.7/|])
+    $ FixtureArtifact "https://github.com/python-poetry/poetry/archive/72497bcb66b5a1cc20e3aa95973c523a22b05bfa.tar.gz" [reldir|python/poetry/poetry/|] (Just [reldir|poetry-72497bcb66b5a1cc20e3aa95973c523a22b05bfa/|])
 
 testPoetrySrcRepo :: Spec
 testPoetrySrcRepo =
@@ -26,7 +26,7 @@ testPoetrySrcRepo =
         shouldFindProjectOf ("poetry", extractedDir) result
       it "should have expected dependency results" $ \(result, extractedDir) -> do
         print result
-        (DependencyResultsSummary 81 0 105 1 Complete) `expectDepResultsSummary` (getDepResultsOf result ("poetry", extractedDir))
+        (DependencyResultsSummary 66 29 69 1 Complete) `expectDepResultsSummary` (getDepResultsOf result ("poetry", extractedDir))
 
 
 spec :: Spec
