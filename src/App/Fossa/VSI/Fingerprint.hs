c04e4b03197601de1a7a68103f172ebbc9c78368
Kit Martin <kit@fossa.com>
2021-12-01 14:17:47 -0800

    Add comment stripped implementation

--- src/App/Fossa/VSI/Fingerprint.hs
+++ src/App/Fossa/VSI/Fingerprint.hs
@@ -16,20 +16,15 @@ import Control.Algebra (Has)
 import Control.Effect.Diagnostics (Diagnostics, fatalText)
 import Control.Effect.Exception (IOException, Lift, catch)
 import Control.Effect.Lift (sendIO)
-import Crypto.Hash (
-  Digest,
-  HashAlgorithm,
-  SHA256,
-  hashFinalize,
-  hashInit,
-  hashUpdate,
- )
+import Crypto.Hash (Digest, HashAlgorithm, SHA256 (..), hashFinalize, hashInit, hashUpdate, hashWith)
 import Data.Aeson (ToJSON, object, toJSON, (.=))
 import Data.ByteString qualified as B
 import Data.String.Conversion (ToText (..))
 import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.Encoding qualified as TE
 import Discovery.Walk (WalkStep (..), walk')
-import Effect.ReadFS (ReadFS)
+import Effect.ReadFS (ReadFS, contentIsBinary, readContentsText)
 import Path (Abs, Dir, File, Path, toFilePath)
 
 -- | Fingerprint deterministically idenfies a file and is derived from its content.
@@ -66,6 +61,9 @@ instance ToJSON Combined where
       , "comment_stripped:sha_256" .= fmap toText combinedCommentStripped
       ]
 
+encodeFingerprint :: Digest hash -> Fingerprint t
+encodeFingerprint = Fingerprint . toText . show
+
 -- | Hashes a stream of 'B.ByteString'@s@ and creates a digest @d@.
 -- Adapted from @sinkHash@ in https://hackage.haskell.org/package/cryptonite-conduit-0.2.2/docs/src/Crypto-Hash-Conduit.html
 sinkHash :: (Monad m, HashAlgorithm hash) => ConduitT B.ByteString Void m (Digest hash)
@@ -87,15 +85,55 @@ hashFile fp =
 fingerprintRaw :: (Has (Lift IO) sig m, Has Diagnostics sig m) => Path Abs File -> m (Fingerprint Raw)
 fingerprintRaw file = do
   (fp :: Digest SHA256) <- hashFile $ toFilePath file
-  pure . Fingerprint . toText $ show fp
+  pure $ encodeFingerprint fp
 
 fingerprintContentsRaw :: (Has ReadFS sig m, Has Diagnostics sig m, Has (Lift IO) sig m) => Path Abs Dir -> m [Fingerprint Raw]
 fingerprintContentsRaw = walk' $ \_ _ files -> do
   fps <- traverse fingerprintRaw files
   pure (fps, WalkContinue)
 
-fingerprintCommentStripped :: (Has (Lift IO) sig m, Has Diagnostics sig m) => Path Abs File -> m (Maybe (Fingerprint CommentStripped))
-fingerprintCommentStripped file = undefined
+fingerprintCommentStripped :: (Has ReadFS sig m, Has Diagnostics sig m) => Path Abs File -> m (Maybe (Fingerprint CommentStripped))
+fingerprintCommentStripped file = contentIsBinary file >>= doFingerprint
+  where
+    doFingerprint True = pure Nothing -- Don't attempt to comment strip binary files
+    doFingerprint False = do
+      content <- readContentsText file
+      pure . Just . encodeFingerprint . hashWith SHA256 . TE.encodeUtf8 $ basicCStyleCommentStrip content
 
-fingerprint :: (Has (Lift IO) sig m, Has Diagnostics sig m) => Path Abs File -> m (Combined)
-fingerprint file = undefined
+fingerprint :: (Has ReadFS sig m, Has (Lift IO) sig m, Has Diagnostics sig m) => Path Abs File -> m Combined
+fingerprint file =
+  Combined
+    <$> fingerprintRaw file
+    <*> fingerprintCommentStripped file
+
+-- | This implementation is based on the comment strip logic from the internal logic used when crawling OSS components.
+-- It is very basic, only works for C-style comments and @\n@ newlines, and doesn't handle edge cases.
+-- Despite these drawbacks, we have to reimplement it the same way so that fingerprints line up correctly in the VSI analysis service.
+--
+-- TODO: We should ideally move this to stream processing instead of buffering the whole file.
+basicCStyleCommentStrip :: Text -> Text
+basicCStyleCommentStrip content = Text.unlines . (process False) $ Text.lines content
+  where
+    breakOn' :: Text -> Text -> (Text, Text, Bool)
+    breakOn' needle haystack = do
+      let (before, remaining) = Text.breakOn needle haystack
+      if remaining == Text.empty -- `remaining` includes the needle if found, so if it's empty it wasn't found
+        then (before, Text.empty, False)
+        else (before, Text.drop (Text.length needle) remaining, True)
+
+    mergeLineWithRest :: Bool -> Text -> [Text] -> [Text]
+    mergeLineWithRest inMultilineComment line remainingLines =
+      if line == Text.empty
+        then process inMultilineComment remainingLines
+        else line : process inMultilineComment remainingLines
+
+    process :: Bool -> [Text] -> [Text]
+    process _ [] = []
+    process True (line : remainingLines) = do
+      let (_, lineAfterComment, foundCommentEnd) = breakOn' "*/" line
+      mergeLineWithRest (not foundCommentEnd) lineAfterComment remainingLines
+    process False (line : remainingLines) = do
+      let (lineBeforeComment, _, foundCommentStart) = breakOn' "/*" line
+      if foundCommentStart
+        then mergeLineWithRest True lineBeforeComment remainingLines
+        else fst (Text.breakOn line "//") : process False remainingLines -- Trim off any single line comment at the end of the line
