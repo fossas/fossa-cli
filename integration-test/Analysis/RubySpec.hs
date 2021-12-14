709bb3c1c955022a04dc123b17c374e7143e5d81
meghfossa <86321858+meghfossa@users.noreply.github.com>
2021-12-14 07:44:16 -0700

    Adds bundler integration test (#732)

---
+++ integration-test/Analysis/RubySpec.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Analysis.RubySpec (spec) where
+
+import Analysis.FixtureExpectationUtils
+import Analysis.FixtureUtils
+import Path
+import Strategy.Bundler qualified as Bundler
+import Test.Hspec
+import Types (GraphBreadth (Complete))
+
+rails :: AnalysisTestFixture (Bundler.BundlerProject)
+rails =
+  AnalysisTestFixture
+    "rails"
+    Bundler.discover
+    LocalEnvironment
+    Nothing
+    $ FixtureArtifact
+      "https://github.com/rails/rails/archive/refs/tags/v6.1.4.tar.gz"
+      [reldir|ruby/rails/|]
+      [reldir|rails-6.1.4/|]
+
+spec :: Spec
+spec = do
+  testSuiteDepResultSummary rails "bundler" (DependencyResultsSummary 210 70 293 1 Complete)
