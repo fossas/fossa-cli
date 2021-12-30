159c7c767190f9f921327268bd91e305fa4d8854
Wesley Van Melle <van.melle.wes@gmail.com>
2021-12-30 14:08:46 -0800

    Add commands to main

--- src/App/NewFossa/Main.hs
+++ src/App/NewFossa/Main.hs
@@ -4,10 +4,15 @@ module App.NewFossa.Main (appMain) where
 
 import App.Fossa.Analyze qualified as Analyze
 import App.Fossa.Container qualified as Container
+import App.Fossa.EmbeddedBinary qualified as Embed
+import App.Fossa.ListTargets qualified as ListTargets
 import App.Fossa.Report qualified as Report
 import App.Fossa.Test qualified as Test
+import App.Fossa.VPS qualified as VPS
+import App.Fossa.VSI.IAT.AssertUserDefinedBinaries qualified as LinkBins
 import App.NewFossa.Subcommand (GetSeverity, SubCommand (..), runSubCommand)
 import App.Version (fullVersionDescription)
+import Control.Concurrent.CGroup (initRTSThreads)
 import Control.Monad (join)
 import Data.String.Conversion (toString)
 import Options.Applicative (
@@ -23,7 +28,6 @@ import Options.Applicative (
   help,
   helpShowGlobals,
   helper,
-  hidden,
   hsubparser,
   info,
   infoOption,
@@ -39,13 +43,15 @@ import Options.Applicative (
  )
 
 appMain :: IO ()
-appMain = join $ customExecParser mainPrefs $ info (subcommands <**> helper <**> versionOpt) progData
+appMain = do
+  initRTSThreads
+  join $ customExecParser mainPrefs $ info (subcommands <**> helper <**> versionOpt) progData
 
 versionOpt :: Parser (a -> a)
 versionOpt =
   infoOption (toString fullVersionDescription) $
     mconcat
-      [long "version", short 'V', help "show version information", hidden]
+      [long "version", short 'V', help "show version information and exit"]
 
 progData :: InfoMod (IO ())
 progData = fullDesc <> header "fossa-cli - Flexible, performant dependency analysis"
@@ -58,6 +64,10 @@ subcommands =
       , decodeSubCommand Test.testSubCommand
       , decodeSubCommand Report.reportSubCommand
       , decodeSubCommand Container.containerSubCommand
+      , decodeSubCommand VPS.vpsSubCommand
+      , decodeSubCommand ListTargets.listSubCommand
+      , decodeSubCommand Embed.dumpSubCommand
+      , decodeSubCommand LinkBins.linkBinsSubCommand
       , initCommand
       ]
 
