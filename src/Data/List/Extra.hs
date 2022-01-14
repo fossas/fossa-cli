3d3aabe0e7ed79ba67c5e472c82b2437cc20012b
Wesley Van Melle <van.melle.wes@gmail.com>
2022-01-14 12:17:42 -0800

    Add functions I wish I had

--- src/Data/List/Extra.hs
+++ src/Data/List/Extra.hs
@@ -1,4 +1,4 @@
-module Data.List.Extra ((!?)) where
+module Data.List.Extra ((!?),head',singleton) where
 
 -- | infix operator to access an array (similar to 'Data.List.!!')
 -- if the index doesn't exist, return Nothing, otherwise
@@ -7,3 +7,13 @@ module Data.List.Extra ((!?)) where
 xs !? ix = case drop ix xs of
   [] -> Nothing
   a : _ -> Just a
+
+-- | If the list is empty, return Nothing, otherwise return the first element
+head' :: [a] -> Maybe a
+head' = \case
+  [] -> Nothing
+  (x:_) -> Just x
+
+-- | Create a one-item list from the item given
+singleton :: a -> [a]
+singleton = (: [])
