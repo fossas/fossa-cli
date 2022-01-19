2aea17f0f64dc8dda2dd3fa65b3100e4eec6c996
Kit Martin <kit@fossa.com>
2022-01-19 10:58:57 -0800

    Fix `isSetUID` Windows implementation

--- src/App/Fossa/VSI/DynLinked/Util.hs
+++ src/App/Fossa/VSI/DynLinked/Util.hs
@@ -4,29 +4,28 @@ module App.Fossa.VSI.DynLinked.Util (
   isSetUID,
 ) where
 
-import Control.Algebra (Has)
-import Control.Effect.Diagnostics (Diagnostics)
-import Control.Effect.Lift (Lift)
+import Path (File, Path)
 
 #ifdef mingw32_HOST_OS
 
 -- | Test whether the file has a `setuid` bit.
 -- Windows doesn't have the concept of a "set uid bit", so always eval to false.
-isSetUID :: (Has (Lift IO) sig m, Has Diagnostics sig m) => Path t File -> m Bool
+isSetUID :: Monad m => Path t File -> m Bool
 isSetUID _ = pure False
 
 #else
 
-import Control.Effect.Diagnostics qualified as Diag
-import Control.Effect.Lift qualified as EL -- qualified to get the linter to stop yelling about combining imports
+import Control.Algebra (Has)
+import Control.Effect.Diagnostics (Diagnostics, fatalOnSomeException)
+import Control.Effect.Lift (Lift, sendIO)
 import Data.Bits ((.|.))
-import Path (File, Path, toFilePath)
+import Path qualified as P
 import System.Posix.Files (fileMode, getFileStatus, setUserIDMode)
 
 -- | Test whether the file has a `setuid` bit.
 isSetUID :: (Has (Lift IO) sig m, Has Diagnostics sig m) => Path t File -> m Bool
 isSetUID file = do
-  stat <- Diag.fatalOnSomeException "get file status" . EL.sendIO . getFileStatus $ toFilePath file
+  stat <- fatalOnSomeException "get file status" . sendIO . getFileStatus $ P.toFilePath file
   pure $ fileMode stat .|. setUserIDMode == 0
 
 #endif
