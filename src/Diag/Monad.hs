7d41111e2bd5fe10e4a15678f1f178bd45b0064e
Connor James <connor@fossa.com>
2022-01-21 10:38:22 -0800

    Add haddocks to Diag.Monad base result operations

--- src/Diag/Monad.hs
+++ src/Diag/Monad.hs
@@ -89,43 +89,51 @@ ResultT ma <||> ResultT ma' = ResultT $ do
     Failure ws eg -> do
       resA' <- ma'
       case resA' of
-        Success ws' a' -> pure (Success (ws' <> failureToWarnings ws eg) a')
+        Success ws' a' -> pure (Success (ws' <> (errGroupToWarning eg : ws)) a')
         Failure ws' eg' -> pure (Failure (ws' <> ws) (eg' <> eg))
 {-# INLINE (<||>) #-}
 
 ---------- Base Result operations
 
+-- | Fail with the given stacktrace and error
 fatalR :: Stack -> SomeErr -> Result a
 fatalR stack e = Failure [] (ErrGroup [] [] (ErrWithStack stack e NE.:| []))
 {-# INLINE fatalR #-}
 
+-- | Emit a standalone warning
 warnR :: SomeWarn -> Result ()
 warnR w = Success [StandaloneWarn w] ()
 {-# INLINE warnR #-}
 
+-- | Recover from a possibly-failing computation, returning @Nothing@ on Failure
+-- and @Just a@ on Success
 recoverR :: Result a -> Result (Maybe a)
-recoverR (Failure ws eg) = Success (failureToWarnings ws eg) Nothing
+recoverR (Failure ws eg) = Success (errGroupToWarning eg : ws) Nothing
 recoverR (Success ws a) = Success ws (Just a)
 {-# INLINE recoverR #-}
 
+-- | Attach a warning to the ErrGroup of a possibly-failing computation. No-op
+-- on Success
 withWarnR :: SomeWarn -> Result a -> Result a
 withWarnR w (Failure ws (ErrGroup sws ectx es)) = Failure ws (ErrGroup (w : sws) ectx es)
 withWarnR _ (Success ws a) = Success ws a
 {-# INLINE withWarnR #-}
 
+-- | Attach error context to the ErrGroup of a possibly-failing computation.
+-- No-op on Success
 errCtxR :: ErrCtx -> Result a -> Result a
 errCtxR c (Failure ws (ErrGroup sws ectx es)) = Failure ws (ErrGroup sws (c : ectx) es)
 errCtxR _ (Success ws a) = Success ws a
 {-# INLINE errCtxR #-}
 
--- Turn (emitted warnings * errgroup) into (emitted warnings)
+-- | Convert an ErrGroup into an EmittedWarn, for the purpose of emitting a
+-- warning when recovering from a Failure
 --
--- The current semantics are that the ErrGroup gets prepended to the list of
--- warnings iff warnings are attached to it; otherwise it's dropped
-failureToWarnings :: [EmittedWarn] -> ErrGroup -> [EmittedWarn]
-failureToWarnings ws (ErrGroup sws ectx es) =
-  -- FIXME: errors from errgroup get dropped when no warning is present. Are these the semantics we want?
+-- When the ErrGroup contains no warnings, this produces 'IgnoredErrGroup'.
+-- Otherwise, this produces 'WarnOnErrGroup'
+errGroupToWarning :: ErrGroup -> EmittedWarn
+errGroupToWarning (ErrGroup sws ectx es) =
   case NE.nonEmpty sws of
-    Nothing -> ws
-    Just sws' -> (WarnOnErrGroup sws' ectx es : ws)
-{-# INLINE failureToWarnings #-}
+    Nothing -> IgnoredErrGroup ectx es
+    Just sws' -> (WarnOnErrGroup sws' ectx es)
+{-# INLINE errGroupToWarning #-}
