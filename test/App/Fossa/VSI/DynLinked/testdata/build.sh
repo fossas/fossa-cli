e7dcd75953e4f2e1844eadb74948c75ca258ffd3
Kit Martin <kit@fossa.com>
2022-01-20 16:27:53 -0800

    Revert "Check in testing binaries"
    This reverts commit 4bff2e59cefbd1e140a268abfd44af801d5a09ba.

--- test/App/Fossa/VSI/DynLinked/testdata/build.sh
+++ test/App/Fossa/VSI/DynLinked/testdata/build.sh
@@ -4,8 +4,6 @@
 set -e
 
 # this script assumes it's running from the root of the project (e.g. via `make`).
-# The binaries are checked in to git so that everyone doesn't have to run this script,
-# but I wanted to include the build artifacts anyway.
 
 ROOT=test/App/Fossa/VSI/DynLinked/testdata
 
