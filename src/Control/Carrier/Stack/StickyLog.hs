ea2170b104d9bf80c161037e1241771c7d3d983e
Connor James <connor@fossa.com>
2022-01-28 10:18:08 -0800

    Use explicit imports in StickyLog

--- src/Control/Carrier/Stack/StickyLog.hs
+++ src/Control/Carrier/Stack/StickyLog.hs
@@ -6,15 +6,16 @@ module Control.Carrier.Stack.StickyLog (
 ) where
 
 import Console.Sticky qualified as Sticky
-import Control.Carrier.Reader
-import Control.Effect.AtomicCounter
-import Control.Effect.Lift
-import Control.Effect.Stack
+import Control.Algebra (Algebra (..), Has, type (:+:) (..))
+import Control.Carrier.Reader (ReaderC, asks, runReader)
+import Control.Effect.AtomicCounter (AtomicCounter, generateId)
+import Control.Effect.Lift (Lift)
+import Control.Effect.Stack (Stack (Context), getStack)
 import Control.Effect.Sum (Member (inj))
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Trans.Class (MonadTrans (..))
 import Data.List (intersperse)
-import Effect.Logger
+import Effect.Logger (Color (Green, Yellow), Logger, Pretty (pretty), Severity (SevDebug), annotate, color, hcat)
 
 stickyLogStack :: (Has AtomicCounter sig m, Has (Lift IO) sig m) => StickyLogStackC m a -> m a
 stickyLogStack act = do
@@ -39,7 +40,7 @@ instance (Algebra sig m, Member Stack sig, Member (Lift IO) sig, Member Logger s
       region <- asks ctxRegion
       stack <- getStack
       let chevron = annotate (color Yellow) (pretty (" > " :: String))
-      let path = map pretty (reverse (txt:stack))
+      let path = map pretty (reverse (txt : stack))
       let formatted = "[" <> annotate (color Green) ("TASK " <> pretty taskId) <> "] " <> hcat (intersperse chevron path)
       Sticky.setSticky' region formatted
       alg (runStickyLogStackC . hdl) (inj thing) ctx
