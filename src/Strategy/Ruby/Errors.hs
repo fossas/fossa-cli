05f4cd42f2bfc766c911358278fe5f1cb12f58ac
Megh <megh@fossa.com>
2022-02-11 16:36:35 -0700

    Ruby: adds contextual error/warns

---
+++ src/Strategy/Ruby/Errors.hs
@@ -0,0 +1,45 @@
+module Strategy.Ruby.Errors (
+  BundlerMissingLockFile (..),
+  RubyMissingEdges (..),
+  RubyMissingDepClassification (..),
+
+  -- * Reference docs
+  bundlerLockFileRationaleUrl,
+  rubyFossaDocUrl,
+) where
+
+import App.Docs (strategyLangDocUrl)
+import Data.Text (Text)
+import Diag.Diagnostic (ToDiagnostic, renderDiagnostic)
+import Path
+import Prettyprinter (indent, viaShow, vsep)
+
+bundlerLockFileRationaleUrl :: Text
+bundlerLockFileRationaleUrl = "https://bundler.io/rationale.html#sharing-your-application-with-other-developers"
+
+rubyFossaDocUrl :: Text
+rubyFossaDocUrl = strategyLangDocUrl "ruby/ruby.md"
+
+newtype BundlerMissingLockFile = BundlerMissingLockFile (Path Abs File)
+instance ToDiagnostic BundlerMissingLockFile where
+  renderDiagnostic (BundlerMissingLockFile path) =
+    vsep
+      [ "We could not corresponding Gemfile.lock for Gemfile: " <> viaShow path
+      , ""
+      , "Ensure your ruby project is built, and has lockfile prior to running fossa."
+      , "If you are using bundler, you can perform: bundler install."
+      , ""
+      , "Refer to:"
+      , indent 4 $ viaShow bundlerLockFileRationaleUrl
+      , indent 4 $ viaShow rubyFossaDocUrl
+      ]
+
+data RubyMissingEdges = RubyMissingEdges
+instance ToDiagnostic RubyMissingEdges where
+  renderDiagnostic (RubyMissingEdges) =
+    "Could not infer edges between dependencies."
+
+data RubyMissingDepClassification = RubyMissingDepClassification
+instance ToDiagnostic RubyMissingDepClassification where
+  renderDiagnostic (RubyMissingDepClassification) =
+    "Could not classify dependencies between direct or deep. All dependencies will be classified as direct."
