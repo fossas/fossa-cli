e9bca9549ca554ef6b0ae6677cbc8bef117dd4e7
Christopher Sasarak <chris@fossa.com>
2022-02-14 11:24:13 -0600

    Initial test skeleton

--- src/Strategy/Node.hs
+++ src/Strategy/Node.hs
@@ -4,11 +4,14 @@
 
 module Strategy.Node (
   discover,
+  pkgGraph,
+  NodeProject (..),
 ) where
 
 import Algebra.Graph.AdjacencyMap qualified as AM
 import Algebra.Graph.AdjacencyMap.Extra qualified as AME
 import App.Fossa.Analyze.Types (AnalyzeProject (analyzeProject))
+import App.Pathfinder.Types (LicenseAnalyzeProject, licenseAnalyzeProject)
 import Control.Effect.Diagnostics (
   Diagnostics,
   Has,
@@ -24,7 +27,9 @@ import Control.Effect.Diagnostics (
 import Control.Monad (void, (<=<))
 import Data.Glob (Glob)
 import Data.Glob qualified as Glob
-import Data.Map (Map)
+import Data.List.Extra (singleton) -- This is in Data.List in base 4.16
+-- This is in Data.List in base 4.16
+import Data.Map (Map, toList)
 import Data.Map.Strict qualified as Map
 import Data.Maybe (catMaybes, mapMaybe)
 import Data.Set (Set)
@@ -53,7 +58,24 @@ import Effect.ReadFS (
   readContentsJson,
  )
 import GHC.Generics (Generic)
-import Path (Abs, Dir, File, Path, Rel, mkRelFile, parent, (</>))
+import Path
+    ( Abs,
+      Dir,
+      File,
+      Path,
+      Rel,
+      mkRelFile,
+      parent,
+      (</>),
+      Abs,
+      Dir,
+      File,
+      Path,
+      Rel,
+      mkRelFile,
+      parent,
+      toFilePath,
+      (</>) )
 import Strategy.Node.Errors (CyclicPackageJson (CyclicPackageJson), MissingNodeLockFile (MissingNodeLockFile))
 import Strategy.Node.Npm.PackageLock qualified as PackageLock
 import Strategy.Node.PackageJson (
@@ -63,6 +85,7 @@ import Strategy.Node.PackageJson (
   NodePackage (NodePackage),
   PackageJson (..),
   PkgJsonGraph (..),
+  PkgJsonLicense (LicenseText, LicenseObj),
   PkgJsonWorkspaces (unWorkspaces),
   Production,
   pkgFileList,
@@ -76,6 +99,8 @@ import Types (
   DiscoveredProjectType (NpmProjectType, YarnProjectType),
   FoundTargets (ProjectWithoutTargets),
   GraphBreadth (Complete, Partial),
+  LicenseResult (LicenseResult, licensesFound),
+  licenseFile, License (License), LicenseType (UnknownType)
  )
 
 skipJsFolders :: WalkStep
@@ -291,7 +316,36 @@ data NodeProject
   | NPM PkgJsonGraph
   deriving (Eq, Ord, Show, Generic)
 
+instance LicenseAnalyzeProject NodeProject where
+  licenseAnalyzeProject = pure . analyzeLicenses . pkgGraph
+
+analyzeLicenses :: PkgJsonGraph -> [LicenseResult]
+analyzeLicenses (PkgJsonGraph _ graph) = mapMaybe (uncurry mkLicenseResult) . toList $ graph
+
+mkLicenseResult :: Manifest -> PackageJson -> Maybe LicenseResult
+mkLicenseResult
+  manifest
+  pkgJson = constrLicenseResult <$> allLicenses
+    where
+      manifestPath = toFilePath . unManifest $ manifest
+      allLicenses = (singleton <$> packageLicense pkgJson) <> packageLicenses pkgJson
+
+      mkLicense (LicenseText txt) = License UnknownType txt
+      mkLicense (LicenseObj pjlo) = error "Not implemented"
+
+
+      constrLicenseResult licenses = do
+        LicenseResult
+          { licenseFile = manifestPath
+          , licensesFound = map mkLicense licenses
+          }
+
 instance ToJSON NodeProject
+
+pkgGraph :: NodeProject -> PkgJsonGraph
+pkgGraph (Yarn _ pjg) = pjg
+pkgGraph (NPMLock _ pjg) = pjg
+pkgGraph (NPM pjg) = pjg
 
 findWorkspaceRootManifest :: PkgJsonGraph -> Either String Manifest
 findWorkspaceRootManifest PkgJsonGraph{jsonGraph} =
--- test/Node/PackageJsonSpec.hs
+++ test/Node/PackageJsonSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Node.PackageJsonSpec (
   spec,
 ) where
@@ -11,12 +13,23 @@ import DepTypes (
   VerConstraint (CCompatible),
  )
 import GraphUtil (expectDeps, expectDirect, expectEdges)
+import Path (mkAbsFile, toFilePath)
 import Strategy.Node.PackageJson (
+  Manifest (Manifest, unManifest),
   PackageJson (..),
+  PkgJsonLicense (LicenseText),
+  PkgJsonLicenseObj (licenseType),
   PkgJsonWorkspaces (PkgJsonWorkspaces),
-  buildGraph,
+  buildGraph, PkgJsonGraph (PkgJsonGraph, jsonGraph, jsonLookup)
  )
-import Test.Hspec (Spec, describe, it)
+import Test.Hspec (Spec, describe, it, shouldBe, fdescribe)
+import Types (License (License, licenseValue), LicenseResult (LicenseResult, licenseFile, licensesFound), LicenseType (UnknownType))
+import Control.Monad (forM_)
+import App.Pathfinder.Types (LicenseAnalyzeProject(licenseAnalyzeProject))
+import Test.Effect (shouldBe', it')
+import Data.Maybe (catMaybes)
+import qualified Algebra.Graph.AdjacencyMap as AM
+import Strategy.Node (NodeProject(NPM))
 
 mockInput :: PackageJson
 mockInput =
@@ -52,11 +65,85 @@ packageTwo =
     , dependencyTags = Map.empty
     }
 
-spec :: Spec
-spec = do
+graphSpec :: Spec
+graphSpec =
   describe "buildGraph" $ do
     it "should produce expected output" $ do
       let graph = buildGraph mockInput
       expectDeps [packageOne, packageTwo] graph
       expectDirect [packageOne, packageTwo] graph
       expectEdges [] graph
+
+-- License Testing
+
+licenseMock :: PackageJson
+licenseMock =
+  PackageJson
+    { packageName = Nothing
+    , packageVersion = Nothing
+    , packageWorkspaces = PkgJsonWorkspaces []
+    , packageDeps = Map.fromList [("packageOne", "^1.0.0")]
+    , packageDevDeps = Map.fromList [("packageTwo", "^2.0.0")]
+    , packageLicense = Nothing
+    , packageLicenses = Nothing
+    }
+
+mockManifest :: Manifest
+mockManifest = Manifest $(mkAbsFile "/usr/local/foo/package.json")
+
+mockManifestFilePath :: FilePath
+mockManifestFilePath = toFilePath . unManifest $ mockManifest
+
+-- 3-tuple of test name, the package json to analyze and a LicenseResult
+type LicenseTestTriple = (String, PackageJson, [LicenseResult])
+
+mkTestLicenseResult :: [License] -> LicenseResult
+mkTestLicenseResult ls = LicenseResult{licenseFile = mockManifestFilePath, licensesFound = ls}
+
+singleLicense :: LicenseTestTriple
+singleLicense =
+  ("Single License", licenseMock{packageLicense = Just (LicenseText "MIT")}, [mkTestLicenseResult [License UnknownType "MIT"]])
+
+licenseTests :: [LicenseTestTriple]
+licenseTests =
+  [singleLicense]
+
+mkMockPkgJsonGraph :: PackageJson -> PkgJsonGraph
+mkMockPkgJsonGraph pkg =
+  PkgJsonGraph {
+  -- jsonGraph isn't relevant to license detection
+  jsonGraph = AM.empty
+  , jsonLookup = Map.fromList [(mockManifest, pkg)]
+  }
+
+-- make an initial helper function to generate test NodeProjects
+-- begin by doing a test of just the cases for the different types of node projects with
+-- a fixed license. Then add the next different dimension (e.g. license text or license obj)
+-- keep adding dimensions until you get to a comprehensive test.
+
+-- remember that most of the fields in a NodeProject/PackageJson are not of interest to us.
+licenseSpec :: Spec
+licenseSpec =
+  fdescribe "license field generation from PackageJson" $ do
+  it' "It discovers a license field" $ do
+    foundLicenses <- licenseAnalyzeProject (NPM $ mkMockPkgJsonGraph licenseMock {packageLicense = Just (LicenseText "MIT") })
+    foundLicenses `shouldBe'` [mkTestLicenseResult [License UnknownType "MIT"]]
+
+  -- do license <- [Just (LicenseText "MIT")]
+     -- [it' ("It correctly detects a license field of " <> show license) $ do
+     --     foundLicenses <- licenseAnalyzeProject (licenseMock {packageLicense = license})
+     --     pure ()]
+       --foundLicenses `shouldBe` mkTestLicenseResult [(catMaybes [License UnknownType <$> license])]
+
+
+
+  -- forM_ licenseTests $
+  --   \(testCaseName, mock, result) ->
+  --     it' ("It generates licenses in the " <> testCaseName <> "case") $ do
+  --     foundLicenses <- licenseAnalyzeProject mock
+  --     foundLicenses `shouldBe'` result
+
+spec :: Spec
+spec = do
+  graphSpec
+  licenseSpec
