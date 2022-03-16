ba6341520cb3ff3b4f3dcd8aec730e4cd5512550
spatten <scott@fossa.com>
2022-03-16 13:21:10 -0700

    clean up src/App/Fossa/LicenseScanner.hs

--- src/App/Fossa/LicenseScanner.hs
+++ src/App/Fossa/LicenseScanner.hs
@@ -13,10 +13,16 @@ import Control.Carrier.Diagnostics qualified as Diag
 import Control.Carrier.StickyLogger (StickyLogger, logSticky)
 import Control.Effect.Lift
 
-import App.Fossa.ArchiveUploader
+import App.Fossa.ArchiveUploader (
+  VendoredDependency (..),
+  arcToLocator,
+  duplicateFailureBundle,
+  duplicateNames,
+  forceVendoredToArchive,
+ )
 import App.Fossa.FossaAPIV1 qualified as Fossa
 import Control.Effect.Diagnostics (Diagnostics, context, fatalText)
-import Control.Monad (unless)
+import Control.Monad (unless, void)
 import Crypto.Hash (Digest, MD5, hash)
 import Data.ByteString qualified as B
 import Data.List (nub)
@@ -59,7 +65,15 @@ runThemis themisBins scanDir = do
 md5 :: B.ByteString -> Digest MD5
 md5 = hash
 
-scanAndUploadVendoredDep :: (Has Diag.Diagnostics sig m, Has (Lift IO) sig m, Has StickyLogger sig m) => ApiOpts -> Path Abs Dir -> VendoredDependency -> m Archive
+scanAndUploadVendoredDep ::
+  ( Has Diag.Diagnostics sig m
+  , Has (Lift IO) sig m
+  , Has StickyLogger sig m
+  ) =>
+  ApiOpts ->
+  Path Abs Dir ->
+  VendoredDependency ->
+  m Archive
 scanAndUploadVendoredDep apiOpts baseDir VendoredDependency{..} = context "compressing and uploading vendored deps" $ do
   logSticky $ "License Scanning '" <> vendoredName <> "' at '" <> vendoredPath <> "'"
   vendoredDepDir <- case parseRelDir $ toString vendoredPath of
@@ -75,19 +89,27 @@ scanAndUploadVendoredDep apiOpts baseDir VendoredDependency{..} = context "compr
           }
 
   depVersion <- case vendoredVersion of
-    Nothing -> pure $ toText $ show $ md5 $ encodeUtf8 $ show themisScanResult
+    Nothing -> pure $ toText . show . md5 . encodeUtf8 . show $ themisScanResult
     Just version -> pure version
 
   signedURL <- Fossa.getSignedLicenseScanURL apiOpts depVersion vendoredName
 
   logSticky $ "Uploading '" <> vendoredName <> "' to secure S3 bucket"
-  _ <- Fossa.licenseScanResultUpload signedURL licenseSourceUnit
+  void $ Fossa.licenseScanResultUpload signedURL licenseSourceUnit
 
   pure $ Archive vendoredName depVersion
 
--- archiveUploadSourceUnit receives a list of vendored dependencies, a root path, and API settings.
+-- licenseScanSourceUnit receives a list of vendored dependencies, a root path, and API settings.
 -- Using this information, it license scans each vendored dependency, uploads the license scan results and then queues a build for the dependency.
-licenseScanSourceUnit :: (Has Diag.Diagnostics sig m, Has (Lift IO) sig m, Has StickyLogger sig m) => Path Abs Dir -> ApiOpts -> [VendoredDependency] -> m [Locator]
+licenseScanSourceUnit ::
+  ( Has Diag.Diagnostics sig m
+  , Has (Lift IO) sig m
+  , Has StickyLogger sig m
+  ) =>
+  Path Abs Dir ->
+  ApiOpts ->
+  [VendoredDependency] ->
+  m [Locator]
 licenseScanSourceUnit baseDir apiOpts vendoredDeps = do
   -- Users with many instances of vendored dependencies may accidentally have complete duplicates. Remove them.
   let uniqDeps = nub vendoredDeps
@@ -102,7 +124,7 @@ licenseScanSourceUnit baseDir apiOpts vendoredDeps = do
 
   -- archiveBuildUpload takes archives without Organization information. This orgID is appended when creating the build on the backend.
   -- We don't care about the response here because if the build has already been queued, we get a 401 response.
-  _ <- Fossa.licenseScanFinalize apiOpts $ ArchiveComponents archives
+  void $ Fossa.licenseScanFinalize apiOpts $ ArchiveComponents archives
 
   -- The organizationID is needed to prefix each locator name. The FOSSA API automatically prefixes the locator when queuing the build
   -- but not when reading from a source unit.
