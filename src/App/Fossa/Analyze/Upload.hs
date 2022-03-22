df32a49f40f2fc4124ee6bc553bed1ffdbcb5f00
Drew Haven <drew@fossa.com>
2022-03-22 10:39:53 -0700

    Moves git access into an effect for easier testing (#846)
    * Moves Git operations into an effect.
    
    * Formatting
    
    * Removes unused exports.

--- src/App/Fossa/Analyze/Upload.hs
+++ src/App/Fossa/Analyze/Upload.hs
@@ -21,6 +21,7 @@ import Control.Effect.Diagnostics (
   fromMaybeText,
   recover,
  )
+import Control.Effect.Git (Git, fetchGitContributors)
 import Control.Effect.Lift (Lift)
 import Data.Aeson ((.=))
 import Data.Aeson qualified as Aeson
@@ -31,7 +32,6 @@ import Data.List.NonEmpty qualified as NE
 import Data.Maybe (fromMaybe)
 import Data.String.Conversion (decodeUtf8)
 import Data.Text (Text)
-import Effect.Exec (Exec, runExecIO)
 import Effect.Logger (
   Has,
   Logger,
@@ -48,12 +48,12 @@ import Srclib.Types (
   SourceUnit,
   parseLocator,
  )
-import VCS.Git (fetchGitContributors)
 
 uploadSuccessfulAnalysis ::
   ( Has Diagnostics sig m
   , Has Logger sig m
   , Has (Lift IO) sig m
+  , Has Git sig m
   ) =>
   BaseDir ->
   ApiOpts ->
@@ -85,7 +85,7 @@ uploadSuccessfulAnalysis (BaseDir basedir) apiOpts metadata jsonOutput revision 
     ]
   traverse_ (\err -> logError $ "FOSSA error: " <> viaShow err) (uploadError uploadResult)
   -- Warn on contributor errors, never fail
-  void . recover . runExecIO $ tryUploadContributors basedir apiOpts (uploadLocator uploadResult)
+  void . recover $ tryUploadContributors basedir apiOpts (uploadLocator uploadResult)
 
   if fromFlag JsonOutput jsonOutput
     then do
@@ -106,7 +106,7 @@ dieOnMonorepoUpload apiOpts revision = do
 
 tryUploadContributors ::
   ( Has Diagnostics sig m
-  , Has Exec sig m
+  , Has Git sig m
   , Has (Lift IO) sig m
   ) =>
   Path Abs Dir ->
