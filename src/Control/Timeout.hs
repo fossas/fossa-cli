0403afc9a8a67977cd13a56d8e815610fffefff8
Wesley Van Melle <wes@fossa.com>
2022-03-28 20:11:15 -0700

    Prevent VSCode from refining duration imports (#860)
    * Prevent VSCode from refining duration imports
    
    * fmt
    
    * Do I even exist?  Am I a typo?
    
    Co-authored-by: Kit Martin <kit@kitified.dev>
    
    Co-authored-by: Kit Martin <kit@kitified.dev>

--- src/Control/Timeout.hs
+++ src/Control/Timeout.hs
@@ -28,12 +28,37 @@ import Control.Effect.Lift (Has, Lift, sendIO)
 import Control.Monad (when)
 import Control.Timeout.Internal (
   Cancel (..),
-  Duration (..),
-  durationToMicro,
  )
 import Data.Functor (($>))
 
--- Whether a cancellable should cancel right now, or poll again.
+-- | Several time-based functions accept 'Int', which makes it difficult to
+-- reason about which subdivision of time that int represents.  To aid this,
+-- our timeout functions accept 'Duration' which provides semantically-obvious
+-- constructors, and is exported to the correct resolution when passed to its
+-- underlying function.
+data Duration
+  = Seconds Int
+  | Minutes Int
+  | MilliSeconds Int
+  | MicroSeconds Int
+  deriving (Show)
+
+instance Eq Duration where
+  a == b = durationToMicro a == durationToMicro b
+
+instance Ord Duration where
+  compare a b = compare (durationToMicro a) (durationToMicro b)
+
+-- | threadDelay only accepts microseconds, so we simplfy that with the tiny
+-- abstraction of 'Duration'.
+durationToMicro :: Duration -> Int
+durationToMicro = \case
+  Seconds n -> n * 1_000_000
+  Minutes n -> n * 60_000_000
+  MilliSeconds n -> n * 1000
+  MicroSeconds n -> n
+
+-- | Whether a cancellable should cancel right now, or poll again.
 -- Use checkForCancel instead if you want to fail.
 shouldCancelRightNow :: Cancel -> IO Bool
 shouldCancelRightNow (Cancel mvar) = not <$> isEmptyMVar mvar
@@ -60,7 +85,7 @@ checkForCancel err cancel = do
 -- The intended usage is:
 -- @
 -- timeout' duration $ \cancelToken -> do
---   longRunningComputation cancelFlag
+--   longRunningComputation cancelToken
 -- @
 --
 -- Which will run @longRunningComputation@ until it returns, and set the cancel
@@ -86,7 +111,7 @@ timeout' duration act = do
   -- requires the timeout to fully expire.
   act (Cancel cancel) `finally` kill handle
 
--- Legacy timeout function, using external cancellation, but cannot work with
+-- | Legacy timeout function, using external cancellation, but cannot work with
 -- other IO-capable monad stacks or effects.
 timeoutIO ::
   -- | number of seconds before timeout
