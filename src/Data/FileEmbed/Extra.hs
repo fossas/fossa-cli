bb47ee5a511f58893f51d4c70a859aad4f21252b
spatten <scott@fossa.com>
2022-04-15 00:22:24 -0700

    get rid of that comment

--- src/Data/FileEmbed/Extra.hs
+++ src/Data/FileEmbed/Extra.hs
@@ -14,7 +14,6 @@ embedFileIfExists inputPath = do
   skipEmbedEnvVar <- runIO $ lookupEnv "FOSSA_SKIP_EMBEDDED_FILES_IN_HLS"
   case (skipEmbedEnvVar, parseRelFile inputPath) of
     (Just _, _) -> do
-      -- If you put a warning in here, then you get linter warnings in EmbeddedBinary.hs
       pure (LitE $ StringL "")
     (_, Just path) -> do
       exists <- doesFileExist path
