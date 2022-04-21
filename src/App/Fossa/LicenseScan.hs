5df5b958306a8b1bf4c0f332de6d9b017561514f
Wes Van Melle <van.melle.wes@gmail.com>
2022-04-21 00:09:19 -0700

    Update and add new cmd

--- src/App/Fossa/LicenseScan.hs
+++ src/App/Fossa/LicenseScan.hs
@@ -3,28 +3,104 @@ module App.Fossa.LicenseScan (
 ) where
 
 import App.Fossa.Config.LicenseScan (
+  LicenseScanCommand,
   LicenseScanConfig (..),
-  LicenseScanOpts,
   mkSubCommand,
  )
 import App.Fossa.EmbeddedBinary (withThemisAndIndex)
+import App.Fossa.LicenseScanner (dedupVendoredDeps, scanVendoredDep)
+import App.Fossa.ManualDeps (
+  ManualDependencies (vendoredDependencies),
+  VendoredDependency (vendoredPath),
+  findFossaDepsFile,
+  readFoundDeps,
+ )
 import App.Fossa.RunThemis (execRawThemis)
 import App.Fossa.Subcommand (SubCommand)
-import Control.Effect.Diagnostics (Diagnostics)
-import Control.Effect.Lift (Has, Lift)
+import App.Types (BaseDir (BaseDir))
+import Control.Carrier.StickyLogger (
+  Has,
+  StickyLogger,
+  runStickyLogger,
+ )
+import Control.Effect.Diagnostics (Diagnostics, ToDiagnostic, fromMaybe)
+import Control.Effect.Lift (Lift)
+import Data.Aeson (KeyValue ((.=)), ToJSON (toJSON), object)
+import Data.Aeson qualified as Aeson
+import Data.List.NonEmpty (NonEmpty)
+import Data.List.NonEmpty qualified as NE
 import Data.String.Conversion (decodeUtf8)
+import Data.Traversable (for)
+import Diag.Diagnostic (ToDiagnostic (renderDiagnostic))
 import Effect.Exec (Exec)
-import Effect.Logger (Logger, logStdout)
+import Effect.Logger (Logger, Severity (SevInfo), logStdout)
+import Effect.ReadFS (ReadFS)
+import Path (Abs, Dir, Path)
+import Prettyprinter (vsep)
+import Srclib.Types (LicenseScanType (CliLicenseScanned), LicenseSourceUnit (LicenseSourceUnit))
 
-licenseScanSubCommand :: SubCommand LicenseScanOpts LicenseScanConfig
+data MissingFossaDepsFile = MissingFossaDepsFile
+data NoVendoredDeps = NoVendoredDeps
+
+instance ToDiagnostic MissingFossaDepsFile where
+  renderDiagnostic _ =
+    vsep
+      [ "'fossa experimental-license-scan fossa-deps' requires pointing to a directory with a fossa-deps file."
+      , "The file can have one of the extensions: .yaml .yml .json"
+      ]
+
+instance ToDiagnostic NoVendoredDeps where
+  renderDiagnostic _ = "The 'vendored-dependencies' section of the fossa deps file is empty or missing."
+
+newtype UploadUnits = UploadUnits (NonEmpty LicenseSourceUnit)
+
+instance ToJSON UploadUnits where
+  toJSON (UploadUnits units) = object ["uploadUnits" .= units]
+
+licenseScanSubCommand :: SubCommand LicenseScanCommand LicenseScanConfig
 licenseScanSubCommand = mkSubCommand licenseScanMain
 
 licenseScanMain ::
   ( Has (Lift IO) sig m
   , Has Exec sig m
   , Has Diagnostics sig m
+  , Has ReadFS sig m
   , Has Logger sig m
   ) =>
   LicenseScanConfig ->
   m ()
-licenseScanMain (LicenseScanConfig dir) = logStdout . decodeUtf8 =<< withThemisAndIndex (`execRawThemis` dir)
+licenseScanMain = \case
+  RawPathScan (BaseDir dir) -> logStdout . decodeUtf8 =<< withThemisAndIndex (`execRawThemis` dir)
+  VendoredDepsOutput basedir -> outputVendoredDeps basedir
+
+outputVendoredDeps ::
+  ( Has Diagnostics sig m
+  , Has ReadFS sig m
+  , Has Logger sig m
+  , Has (Lift IO) sig m
+  , Has Exec sig m
+  ) =>
+  BaseDir ->
+  m ()
+outputVendoredDeps (BaseDir dir) = runStickyLogger SevInfo $ do
+  manualDepsFile <- fromMaybe MissingFossaDepsFile =<< findFossaDepsFile dir
+  manualDeps <- readFoundDeps manualDepsFile
+  vendoredDeps <- fromMaybe NoVendoredDeps $ NE.nonEmpty $ vendoredDependencies manualDeps
+  resultMap <- UploadUnits <$> runLicenseScan dir vendoredDeps
+  logStdout . decodeUtf8 $ Aeson.encode resultMap
+
+runLicenseScan ::
+  ( Has Diagnostics sig m
+  , Has ReadFS sig m
+  , Has StickyLogger sig m
+  , Has (Lift IO) sig m
+  , Has Exec sig m
+  ) =>
+  Path Abs Dir ->
+  NonEmpty VendoredDependency ->
+  m (NonEmpty LicenseSourceUnit)
+runLicenseScan basedir vdeps = do
+  uniqDeps <- dedupVendoredDeps vdeps
+  for uniqDeps $ \dep ->
+    LicenseSourceUnit (vendoredPath dep) CliLicenseScanned
+      <$> scanVendoredDep basedir dep
