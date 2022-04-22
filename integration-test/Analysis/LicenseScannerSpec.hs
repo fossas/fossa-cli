4768d0f77adfc463de56f4630efa32d1d5ea836a
spatten <scott@fossa.com>
2022-04-22 13:02:33 -0700

    integration test for nested archives that actually tests something

--- integration-test/Analysis/LicenseScannerSpec.hs
+++ integration-test/Analysis/LicenseScannerSpec.hs
@@ -12,18 +12,21 @@ import App.Fossa.LicenseScanner (scanVendoredDep)
 import Control.Carrier.Diagnostics (runDiagnostics)
 import Control.Carrier.Stack (runStack)
 import Control.Carrier.StickyLogger (runStickyLogger)
+import Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.List.NonEmpty qualified as NE
 import Diag.Result (Result (Failure, Success), renderFailure)
 import Effect.Exec (runExecIO)
 import Effect.Logger (Severity (SevDebug), withDefaultLogger)
 import Effect.ReadFS (runReadFSIO)
 import Path (reldir, (</>))
 import Path.IO qualified as PIO
+import Srclib.Types (LicenseUnit (licenseUnitFiles, licenseUnitName))
 import Test.Hspec (Spec, it, shouldBe)
 
 recursiveArchive :: FixtureArtifact
 recursiveArchive =
   FixtureArtifact
-    "https://github.com/spatten/cli-license-scan-integration-test-fixtures/archive/refs/heads/main.zip"
+    "https://github.com/fossas/cli-license-scan-integration-test-fixtures/archive/refs/heads/main.zip"
     [reldir|manual-deps/license-scanner/|]
     [reldir|recursive-archive|]
 
@@ -34,13 +37,40 @@ vendoredDep =
     "vendor/foo.tar.gz"
     (Just "0.0.1")
 
+-- the contents of the archive look like this. The `FOO_LICENSE` files contain an MIT license.
+-- `bar_apache.rb` and `something.rb` contain an Apache-2.0 license.
+-- The quux directory is compressed into quux.tar.gz, and then baz into baz.tar.gz, bar into bar.tar.gz
+-- and foo into foo.tar.gz
+-- The script that creates this nested archive is here: https://github.com/fossas/example-projects/tree/main/recursive-archive-generator
+--
+-- recursive-archive
+-- └── vendor
+--     └── foo
+--         ├── VERSION
+--         └── bar
+--             ├── MIT_LICENSE
+--             ├── bar_apache.rb
+--             └── baz
+--                 ├── SOMETHING_LICENSE
+--                 ├── quux
+--                 │   └── QUUX_LICENSE
+--                 └── something.rb
 spec :: Spec
 spec = do
-  it "should find licenses" $ do
+  it "should find licenses in nested archives" $ do
     extractedDir <- getArtifact recursiveArchive
     let scanDir = extractedDir </> [reldir|cli-license-scan-integration-test-fixtures-main/recursive-archive|]
     units <- runStack $ runDiagnostics $ withDefaultLogger SevDebug $ runStickyLogger SevDebug $ runExecIO $ runReadFSIO $ scanVendoredDep scanDir vendoredDep
-    -- PIO.removeDirRecur extractedDir
+    PIO.removeDirRecur extractedDir
     case units of
       Failure ws eg -> fail (show (renderFailure ws eg "An issue occurred"))
-      Success _ us -> (length us) `shouldBe` 2
+      Success _ us -> do
+        (length us) `shouldBe` 3
+        NE.sort (NE.map licenseUnitName us) `shouldBe` "No_license_found" :| ["apache-2.0", "mit"]
+        NE.sort (licenseUnitFiles mitUnit) `shouldBe` "vendor/foo/bar/MIT_LICENSE" :| ["vendor/foo/bar/baz/SOMETHING_LICENSE", "vendor/foo/bar/baz/quux/QUUX_LICENSE"]
+        NE.sort (licenseUnitFiles apacheUnit) `shouldBe` "vendor/foo/bar/bar_apache.rb" :| ["vendor/foo/bar/baz/something.rb"]
+        where
+          mitUnit :: LicenseUnit
+          mitUnit = head $ NE.filter (\u -> licenseUnitName u == "mit") us
+          apacheUnit :: LicenseUnit
+          apacheUnit = head $ NE.filter (\u -> licenseUnitName u == "apache-2.0") us
