2a982e16076d6cf64a6f683591abcbf8015bb903
spatten <scott@fossa.com>
2022-05-12 13:01:46 -0700

    add tests for compressFile

---
+++ test/App/Fossa/ArchiveUploaderSpec.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module App.Fossa.ArchiveUploaderSpec
+  ( spec,
+  )
+where
+
+import App.Fossa.ArchiveUploader
+  ( compressFile,
+  )
+import Control.Carrier.Lift (sendIO)
+import Control.Effect.Path (withSystemTempDir)
+import Path (Abs, Dir, Path, mkRelDir, (</>))
+import Path.IO (getCurrentDir)
+import Test.Effect (it', shouldBe', shouldContain')
+import Test.Hspec (Spec, describe, runIO)
+
+spec :: Spec
+spec = do
+  currDir <- runIO getCurrentDir
+  describe "compressFile" $ do
+    it' "should compress a file" $
+      do
+        let fileToTar = "foo"
+        let specDir = currDir </> $(mkRelDir "test/ArchiveUploader/normal")
+        compressedFilePath <- sendIO $ withSystemTempDir "fossa-temp" (flippedCompressFile specDir fileToTar)
+        compressedFilePath `shouldContain'` fileToTar
+
+    -- We are mostly testing that this does not raise an exception
+    it' "should compress a directory called '.' without throwing" $
+      do
+        let fileToTar = "."
+        let specDir = currDir </> $(mkRelDir "test/ArchiveUploader/normal")
+        compressedFilePath <- sendIO $ withSystemTempDir "fossa-temp" (flippedCompressFile specDir fileToTar)
+        compressedFilePath `shouldContain'` fileToTar
+  where
+    flippedCompressFile :: Path Abs Dir -> FilePath -> Path Abs Dir -> IO FilePath
+    flippedCompressFile directory fileToTar outputDir = compressFile outputDir directory fileToTar
