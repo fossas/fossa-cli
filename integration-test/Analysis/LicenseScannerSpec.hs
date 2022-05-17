c663a11ecfcb9b5eca0ae24bdc57cb4758a0d7d3
spatten <scott@fossa.com>
2022-05-16 18:42:59 -0700

    moving those tests into the LicenseScanner unit tests

--- integration-test/Analysis/LicenseScannerSpec.hs
+++ integration-test/Analysis/LicenseScannerSpec.hs
@@ -7,7 +7,7 @@ import Analysis.FixtureUtils (
   FixtureArtifact (FixtureArtifact),
   getArtifact,
  )
-import App.Fossa.LicenseScanner (licenseScanSourceUnit, scanVendoredDep)
+import App.Fossa.LicenseScanner (scanVendoredDep)
 import App.Fossa.VendoredDependency (VendoredDependency (VendoredDependency))
 import Control.Carrier.Diagnostics (Has, runDiagnostics)
 import Control.Carrier.Stack (runStack)
@@ -18,19 +18,15 @@ import Data.List.NonEmpty qualified as NE
 import Data.Maybe (fromMaybe)
 import Diag.Result (Result (Failure, Success), renderFailure)
 import Effect.Exec (runExecIO)
-import Effect.Logger (ignoreLogger, runLogger)
 import Effect.ReadFS (runReadFSIO)
-import Fossa.API.Types (RevisionInfo)
 import Path (reldir, (</>))
 import Path.IO qualified as PIO
 import Srclib.Types (
   LicenseSourceUnit (licenseSourceUnitLicenseUnits),
   LicenseUnit (licenseUnitFiles, licenseUnitName),
-  Locator (Locator),
   emptyLicenseUnit,
  )
 import Test.Hspec (Spec, describe, it, shouldBe)
-import Test.MockApi (MockApi, alwaysReturns, returnsOnce)
 
 recursiveArchive :: FixtureArtifact
 recursiveArchive =
@@ -39,48 +35,12 @@ recursiveArchive =
     [reldir|manual-deps/license-scanner/|]
     [reldir|recursive-archive|]
 
-firstVendoredDep :: VendoredDependency
-firstVendoredDep =
+vendoredDep :: VendoredDependency
+vendoredDep =
   VendoredDependency
     "recursive-archive-test"
     "vendor/foo.tar.gz"
     (Just "0.0.1")
-
-secondVendoredDep :: VendoredDependency
-secondVendoredDep =
-  VendoredDependency
-    "another-archive-test"
-    "vendor/bar.tar.gz"
-    (Just "0.0.1")
-
-vendoredDeps :: NonEmpty VendoredDependency
-vendoredDeps = firstVendoredDep :| [secondVendoredDep]
-
-firstLocator :: Locator
-firstLocator =
-  Locator
-    "archive"
-    "recursive-archive-test"
-    "0.0.1"
-
-secondLocator :: Locator
-secondLocator =
-  Locator
-    "archive"
-    "another-archive-test"
-    "0.0.1"
-
-firstRevision :: RevisionInfo
-firstRevision =
-  Revision
-    "archive+1/recursive-archive-test$0.0.1"
-    True
-
-secondRevision :: RevisionInfo
-secondRevision =
-  Revision
-    "archive+1/another-archive-test$0.0.1"
-    True
 
 -- the contents of the archive look like this. The `FOO_LICENSE` files contain an MIT license.
 -- `bar_apache.rb` and `something.rb` contain an Apache-2.0 license.
@@ -106,7 +66,7 @@ spec = do
     it "should find licenses in nested archives" $ do
       extractedDir <- getArtifact recursiveArchive
       let scanDir = extractedDir </> [reldir|cli-license-scan-integration-test-fixtures-main/recursive-archive|]
-      units <- runStack . runDiagnostics . ignoreStickyLogger . runExecIO . runReadFSIO . fmap licenseSourceUnitLicenseUnits $ scanVendoredDep scanDir firstVendoredDep
+      units <- runStack . runDiagnostics . ignoreStickyLogger . runExecIO . runReadFSIO . fmap licenseSourceUnitLicenseUnits $ scanVendoredDep scanDir vendoredDep
       PIO.removeDirRecur extractedDir
       case units of
         Failure ws eg -> fail (show (renderFailure ws eg "An issue occurred"))
@@ -120,22 +80,3 @@ spec = do
             mitUnit = fromMaybe emptyLicenseUnit (head' $ NE.filter (\u -> licenseUnitName u == "mit") us)
             apacheUnit :: LicenseUnit
             apacheUnit = fromMaybe emptyLicenseUnit (head' $ NE.filter (\u -> licenseUnitName u == "apache-2.0") us)
-
-  describe "licenseScanSourceUnits" $ do
-    it "should skip all if Core knows about all of the revisions" $ do
-      extractedDir <- getArtifact recursiveArchive
-      let scanDir = extractedDir </> [reldir|cli-license-scan-integration-test-fixtures-main/recursive-archive|]
-      expectScannedAlready
-
-      locators <- runStack . ignoreLogger . runDiagnostics . ignoreStickyLogger . runExecIO . runReadFSIO $ licenseScanSourceUnit scanDir vendoredDeps
-      case locators of
-        Failure ws eg -> fail (show (renderFailure ws eg "An issue occurred"))
-        Success _ ls -> do
-          ls `shouldBe` expectedLocators
-  where
-    expectedLocators = firstLocator :| [secondLocator]
-
-expectEverythingScannedAlready :: Has MockApi sig m => m ()
-expectEverythingScannedAlready =
-  (GetRevisionInfo vendoredDeps)
-    `returnsOnce` firstRevision :| [secondRevision]
---
+++ test/App/Fossa/LicenseScanner/LicenseScannerSpec.hs
@@ -0,0 +1,149 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module App.Fossa.LicenseScanner.LicenseScannerSpec (spec) where
+
+import App.Fossa.LicenseScanner (combineLicenseUnits, licenseScanSourceUnit)
+import App.Fossa.ManualDeps (VendoredDependency (..))
+import Control.Algebra (Has)
+import Control.Carrier.Diagnostics (runDiagnostics)
+import Control.Carrier.Stack (runStack)
+import Control.Carrier.StickyLogger (ignoreStickyLogger)
+import Control.Effect.FossaApiClient (FossaApiClientF (..))
+import Control.Effect.Lift (Lift)
+import Control.Monad (void)
+import Control.Timeout (Cancel, Duration (Seconds), timeout')
+import Data.List.NonEmpty (NonEmpty ((:|)))
+import Diag.Result (Result (..), renderFailure)
+import Effect.Exec (runExecIO)
+import Effect.Logger (ignoreLogger)
+import Effect.ReadFS (runReadFSIO)
+import Fossa.API.Types (RevisionInfo (RevisionInfo))
+import Path (reldir, (</>))
+import Path.IO (getCurrentDir)
+import Srclib.Types (
+  LicenseUnit (..),
+  LicenseUnitData (..),
+  LicenseUnitInfo (..),
+  Locator (Locator),
+  emptyLicenseUnitData,
+ )
+import Test.Effect (it', shouldBe')
+import Test.Hspec (Spec, describe, it, runIO, shouldBe)
+import Test.MockApi (MockApi, alwaysReturns, returnsOnce)
+
+-- test data for combineLicenseUnits tests
+info :: LicenseUnitInfo
+info = LicenseUnitInfo{licenseUnitInfoDescription = Just ""}
+
+unitOne :: LicenseUnit
+unitOne =
+  LicenseUnit
+    { licenseUnitName = "MIT"
+    , licenseUnitType = "LicenseUnit"
+    , licenseUnitDir = ""
+    , licenseUnitData = emptyLicenseUnitData{licenseUnitDataPath = "foo/bar/LICENSE"} :| [emptyLicenseUnitData{licenseUnitDataPath = "foo/bar/one.txt"}]
+    , licenseUnitFiles =
+        "foo/bar/LICENSE" :| ["foo/bar/one.txt"]
+    , licenseUnitInfo = info
+    }
+
+unitTwo :: LicenseUnit
+unitTwo =
+  LicenseUnit
+    { licenseUnitName = "MIT"
+    , licenseUnitType = "LicenseUnit"
+    , licenseUnitDir = ""
+    , licenseUnitData = emptyLicenseUnitData{licenseUnitDataPath = "foo/bar/baz/ANOTHER_LICENSE"} :| [emptyLicenseUnitData{licenseUnitDataPath = "foo/bar/baz/two.txt"}]
+    , licenseUnitFiles = "foo/bar/baz/ANOTHER_LICENSE" :| ["foo/bar/baz/two.txt"]
+    , licenseUnitInfo = info
+    }
+expectedCombinedUnit :: LicenseUnit
+expectedCombinedUnit =
+  LicenseUnit
+    { licenseUnitName = "MIT"
+    , licenseUnitType = "LicenseUnit"
+    , licenseUnitDir = ""
+    , licenseUnitData =
+        emptyLicenseUnitData{licenseUnitDataPath = "foo/bar/LICENSE"}
+          :| [ emptyLicenseUnitData{licenseUnitDataPath = "foo/bar/baz/ANOTHER_LICENSE"}
+             , emptyLicenseUnitData{licenseUnitDataPath = "foo/bar/baz/two.txt"}
+             , emptyLicenseUnitData{licenseUnitDataPath = "foo/bar/one.txt"}
+             ]
+    , licenseUnitFiles =
+        "foo/bar/LICENSE"
+          :| [ "foo/bar/baz/ANOTHER_LICENSE"
+             , "foo/bar/baz/two.txt"
+             , "foo/bar/one.txt"
+             ]
+    , licenseUnitInfo = info
+    }
+
+-- Test data for licenseScanSourceUnits tests
+firstVendoredDep :: VendoredDependency
+firstVendoredDep =
+  VendoredDependency
+    "first-archive-test"
+    "vendor/foo"
+    (Just "0.0.1")
+
+secondVendoredDep :: VendoredDependency
+secondVendoredDep =
+  VendoredDependency
+    "second-archive-test"
+    "vendor/bar"
+    (Just "0.0.1")
+
+vendoredDeps :: NonEmpty VendoredDependency
+vendoredDeps = firstVendoredDep :| [secondVendoredDep]
+
+firstLocator :: Locator
+firstLocator =
+  Locator
+    "archive"
+    "first-archive-test"
+    (Just "0.0.1")
+
+secondLocator :: Locator
+secondLocator =
+  Locator
+    "archive"
+    "second-archive-test"
+    (Just "0.0.1")
+
+firstRevision :: RevisionInfo
+firstRevision =
+  RevisionInfo
+    "archive+1/first-archive-test$0.0.1"
+    True
+
+secondRevision :: RevisionInfo
+secondRevision =
+  RevisionInfo
+    "archive+1/second-archive-test$0.0.1"
+    True
+
+spec :: Spec
+spec = do
+  -- this test only exists to prevent merging the commented out analyzers
+  describe "combineLicenseUnits" $ do
+    it "should combine two MIT units" $
+      combineLicenseUnits [unitOne, unitTwo] `shouldBe` [expectedCombinedUnit]
+    it "should not combine two units with different licenses" $
+      combineLicenseUnits [unitOne, unitTwo{licenseUnitName = "AGPL"}] `shouldBe` [unitTwo{licenseUnitName = "AGPL"}, unitOne]
+  describe "licenseScanSourceUnits" $ do
+    currDir <- runIO getCurrentDir
+    it' "should skip all if Core knows about all of the revisions" $ do
+      let scanDir = currDir </> [reldir|testdata/repo|]
+      expectEverythingScannedAlready
+      locators <- runStack . ignoreLogger . runDiagnostics . ignoreStickyLogger . runExecIO . runReadFSIO $ licenseScanSourceUnit scanDir vendoredDeps
+      case locators of
+        Failure ws eg -> False `shouldBe'` True -- fail (show (renderFailure ws eg "An issue occurred"))
+        Success _ ls -> do
+          ls `shouldBe'` expectedLocators
+  where
+    expectedLocators = firstLocator :| [secondLocator]
+
+expectEverythingScannedAlready :: Has MockApi sig m => m ()
+expectEverythingScannedAlready =
+  (GetRevisionInfo vendoredDeps)
+    `returnsOnce` [firstRevision, secondRevision]
