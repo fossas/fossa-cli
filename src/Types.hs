f0c29e4ca5074d8d6f2ba35e40f1145b73d69a05
spatten <scott@fossa.com>
2022-06-08 17:13:25 -0700

    fix a copy/paste error

--- src/Types.hs
+++ src/Types.hs
@@ -279,7 +279,7 @@ instance FromJSON ArchiveUploadType where
   parseJSON = withText "ArchiveUploadType" $ \case
     "ArchiveUpload" -> pure ArchiveUpload
     "CLILicenseScan" -> pure CLILicenseScan
-    notSupported -> fail . toString $ "Expected either: 'ArchiveUpload' or 'CLILicenseScan' for telemetry scope. You provided: " <> notSupported
+    notSupported -> fail . toString $ "Expected either: 'ArchiveUpload' or 'CLILicenseScan' for scanType. You provided: " <> notSupported
 
 instance ToJSON ArchiveUploadType where
   toJSON = toJSON . show
