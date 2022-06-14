12a1e8c4d3fd4077544f31291f755b9858d76179
Leo Zhang <leo@fossa.io>
2022-06-14 16:11:50 -0700

    refactor: Wrap embedFile instead of reimplementing

--- src/Data/FileEmbed/Extra.hs
+++ src/Data/FileEmbed/Extra.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TemplateHaskellQuotes #-}
-
 module Data.FileEmbed.Extra (
   embedFileL,
   embedFileIfExists,
@@ -7,26 +5,16 @@ module Data.FileEmbed.Extra (
 
 import Control.Exception (try)
 import Control.Monad (when)
-import Data.ByteString qualified as B
-import Data.ByteString.Char8 qualified as B8
-import Data.ByteString.Internal qualified as B
-import Data.ByteString.Unsafe (unsafePackAddressLen)
 import Data.FileEmbed (embedFile)
 import Data.Foldable (traverse_)
 import Data.Maybe (isJust)
-import Language.Haskell.TH (bytesPrimL, mkBytes, reportWarning, runIO)
-import Language.Haskell.TH.Syntax (
-  Exp (AppE, LitE, VarE),
-  Lit (..),
-  Q,
-  qAddDependentFile,
- )
+import Language.Haskell.TH (reportWarning, runIO)
+import Language.Haskell.TH.Syntax (Exp (LitE), Lit (..), Q)
 import Path (parseRelFile)
 import Path.IO (doesFileExist)
 import System.Directory (getCurrentDirectory, listDirectory)
 import System.Environment (lookupEnv)
 import System.FilePath (isRelative, splitPath, takeDirectory)
-import System.IO.Unsafe (unsafePerformIO)
 
 embedFileIfExists :: FilePath -> Q Exp
 embedFileIfExists inputPath = do
@@ -46,11 +34,14 @@ embedFileIfExists inputPath = do
 -- | Like `embedFile`, but prints out debug logging when
 -- @FOSSA_DEBUG_EMBED_FILE@ is set.
 embedFileL :: FilePath -> Q Exp
-embedFileL fp = qAddDependentFile fp >> runIO embedWithLogging >>= bsToExp
+embedFileL fp = do
+  _ <- runIO logEmbedFile
+  embedFile fp
   where
-    embedWithLogging = do
+    logEmbedFile :: IO (Either IOError ())
+    logEmbedFile = do
       embedLogEnvVar <- lookupEnv "FOSSA_DEBUG_EMBED_FILE"
-      _ :: Either IOError () <- try $
+      try $
         when (isJust embedLogEnvVar) $ do
           cwd <- getCurrentDirectory
           putStrLn $ "CWD: " <> cwd
@@ -61,24 +52,8 @@ embedFileL fp = qAddDependentFile fp >> runIO embedWithLogging >>= bsToExp
 
           let dirs = take (length (splitPath fp) - if isRel then 0 else 1) $ tail $ iterate takeDirectory fp
           traverse_ ls dirs
-      B.readFile fp
 
     ls :: FilePath -> IO ()
     ls dir = do
       files <- listDirectory dir
       putStrLn $ "ls " <> show dir <> ": " <> show files
-
--- See https://github.com/snoyberg/file-embed/blob/548430d2a79bb6f4cb4256768761071f59909aa5/Data/FileEmbed.hs#L184-L204
-bsToExp :: B.ByteString -> Q Exp
-bsToExp bs =
-  pure $
-    VarE 'unsafePerformIO
-      `AppE` ( VarE 'unsafePackAddressLen
-                `AppE` LitE (IntegerL $ fromIntegral $ B8.length bs)
-                `AppE` LitE
-                  ( bytesPrimL
-                      ( let B.PS ptr off sz = bs
-                         in mkBytes ptr (fromIntegral off) (fromIntegral sz)
-                      )
-                  )
-             )
