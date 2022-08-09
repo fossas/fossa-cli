09796523711dea36ee8dd0d546aa407708cea1b1
Jessica Black <me@jessica.black>
2022-08-09 12:44:18 -0700

    Test for two entries

--- test/Container/DpkgSpec.hs
+++ test/Container/DpkgSpec.hs
@@ -14,7 +14,7 @@ import Path.IO qualified as PIO
 import ResultUtil (assertOnSuccess)
 import Test.Hspec (Expectation, Spec, describe, it, runIO, shouldBe)
 import Test.Hspec.Megaparsec (shouldParse)
-import Text.Megaparsec (Parsec, parse)
+import Text.Megaparsec (Parsec, parse, some)
 import Text.RawString.QQ (r)
 
 spec :: Spec
@@ -29,6 +29,9 @@ spec = do
     it "parses a single realistic entry" $
       singleEntry `shouldParseEntryInto` singleEntryExpected
 
+    it "parses two entries" $
+      doubleEntry `shouldParseEntriesInto` doubleEntryExpected
+
     it "parses var/lib/dpkg/status" $
       assertOnSuccess result $ \_ c ->
         c `shouldBe` expectedEntries
@@ -40,9 +43,12 @@ testDataDir = PIO.resolveDir' "test/Container/testdata/"
 
 shouldParseEntryInto :: Text -> DpkgEntry -> Expectation
 shouldParseEntryInto = parseMatch dpkgEntryParser
-  where
-    parseMatch :: (Show a, Eq a) => Parsec Void Text a -> Text -> a -> Expectation
-    parseMatch parser input expected = parse parser "" input `shouldParse` expected
+
+shouldParseEntriesInto :: Text -> [DpkgEntry] -> Expectation
+shouldParseEntriesInto = parseMatch $ some dpkgEntryParser
+
+parseMatch :: (Show a, Eq a) => Parsec Void Text a -> Text -> a -> Expectation
+parseMatch parser input expected = parse parser "" input `shouldParse` expected
 
 singleEntryExpected :: DpkgEntry
 singleEntryExpected =
@@ -51,6 +57,20 @@ singleEntryExpected =
     , dpkgEntryPackage = "adduser"
     , dpkgEntryVersion = "3.118"
     }
+
+doubleEntryExpected :: [DpkgEntry]
+doubleEntryExpected =
+  [ DpkgEntry
+      { dpkgEntryArch = "all"
+      , dpkgEntryPackage = "adduser"
+      , dpkgEntryVersion = "3.118"
+      }
+  , DpkgEntry
+      { dpkgEntryArch = "arm64"
+      , dpkgEntryPackage = "apt"
+      , dpkgEntryVersion = "2.2.4"
+      }
+  ]
 
 singleEntry :: Text
 singleEntry =
@@ -92,6 +112,75 @@ simpleSingleEntry =
   [r|Package: adduser
 Architecture: all
 Version: 3.118
+|]
+
+doubleEntry :: Text
+doubleEntry =
+  [r|Package: adduser
+Status: install ok installed
+Priority: important
+Section: admin
+Installed-Size: 849
+Maintainer: Debian Adduser Developers <adduser@packages.debian.org>
+Architecture: all
+Multi-Arch: foreign
+Version: 3.118
+Depends: passwd, debconf (>= 0.5) | debconf-2.0
+Suggests: liblocale-gettext-perl, perl
+Conffiles:
+ /etc/deluser.conf 773fb95e98a27947de4a95abb3d3f2a2
+Description: add and remove users and groups
+ This package includes the 'adduser' and 'deluser' commands for creating
+ and removing users.
+ .
+  - 'adduser' creates new users and groups and adds existing users to
+    existing groups;
+  - 'deluser' removes users and groups and removes users from a given
+    group.
+ .
+ Adding users with 'adduser' is much easier than adding them manually.
+ Adduser will choose appropriate UID and GID values, create a home
+ directory, copy skeletal user configuration, and automate setting
+ initial values for the user's password, real name and so on.
+ .
+ Deluser can back up and remove users' home directories
+ and mail spool or all the files they own on the system.
+ .
+ A custom script can be executed after each of the commands.
+
+Package: apt
+Status: install ok installed
+Priority: required
+Section: admin
+Installed-Size: 4181
+Maintainer: APT Development Team <deity@lists.debian.org>
+Architecture: arm64
+Version: 2.2.4
+Replaces: apt-transport-https (<< 1.5~alpha4~), apt-utils (<< 1.3~exp2~)
+Provides: apt-transport-https (= 2.2.4)
+Depends: adduser, gpgv | gpgv2 | gpgv1, libapt-pkg6.0 (>= 2.2.4), debian-archive-keyring, libc6 (>= 2.17), libgcc-s1 (>= 3.0), libgnutls30 (>= 3.7.0), libseccomp2 (>= 2.4.2), libstdc++6 (>= 9), libsystemd0
+Recommends: ca-certificates
+Suggests: apt-doc, aptitude | synaptic | wajig, dpkg-dev (>= 1.17.2), gnupg | gnupg2 | gnupg1, powermgmt-base
+Breaks: apt-transport-https (<< 1.5~alpha4~), apt-utils (<< 1.3~exp2~), aptitude (<< 0.8.10)
+Conffiles:
+ /etc/apt/apt.conf.d/01autoremove ab6540f7278a05a4b7f9e58afcaa5f46
+ /etc/cron.daily/apt-compat 49e9b2cfa17849700d4db735d04244f3
+ /etc/kernel/postinst.d/apt-auto-removal 6486b24d4c496e7d6a443178869a019b
+ /etc/logrotate.d/apt 179f2ed4f85cbaca12fa3d69c2a4a1c3
+Description: commandline package manager
+ This package provides commandline tools for searching and
+ managing as well as querying information about packages
+ as a low-level access to all features of the libapt-pkg library.
+ .
+ These include:
+  * apt-get for retrieval of packages and information about them
+    from authenticated sources and for installation, upgrade and
+    removal of packages together with their dependencies
+  * apt-cache for querying available information about installed
+    as well as installable packages
+  * apt-cdrom to use removable media as a source for packages
+  * apt-config as an interface to the configuration settings
+  * apt-key as an interface to manage authentication keys
 |]
 
 expectedEntries :: [DpkgEntry]
