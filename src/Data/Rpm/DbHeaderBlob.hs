b0a692947eb466d67a2be097d149b11a09f99054
Christopher Sasarak <chris@fossa.com>
2022-08-15 15:35:46 -0500

    Index/data length header blob parse

--- src/Data/Rpm/DbHeaderBlob.hs
+++ src/Data/Rpm/DbHeaderBlob.hs
@@ -1,2 +1,42 @@
-module Data.Rpm.DbHeaderBlob where
+{-# LANGUAGE RecordWildCards #-}
 
+module Data.Rpm.DbHeaderBlob (headerBlobInit
+                             , HeaderBlob(..)) where
+
+import Data.Binary.Get (ByteOffset, getInt32be, runGetOrFail, label, getInt32le)
+import Data.ByteString.Lazy qualified as BLS
+import Data.Int (Int32)
+
+data HeaderBlob = HeaderBlob
+  { indexLength :: Int32
+  , dataLength :: Int32
+  }
+  deriving (Show, Eq)
+
+-- data BlobParseError
+--   = IndexLength
+--   | DataLength
+
+-- data BlobParseContext = BlobParseContext
+--   { byteString :: BLS.ByteString
+--   , offset :: ByteOffset
+--   , error :: BlobParseError
+--   }
+
+-- -- This might raise an error, but is never meant to be used outside this module.
+-- strToError :: String -> BlobParseError
+-- strToError = \case
+--   "indexLen" -> IndexLength
+--   "dataLen" -> DataLength
+--   _ -> Prelude.error "strToError should not be used outside its module. This error indicates a bug in Data.Rpm.DbHeaderBlob."
+
+headerBlobInit :: BLS.ByteString -> Either (BLS.ByteString, ByteOffset, String) HeaderBlob
+headerBlobInit bs = 
+  case runGetOrFail readHeader bs of
+    Right (_, _, res) -> Right res
+    Left l -> Left l
+  where
+    readHeader = do
+      indexLength <- label "Read indexLength" getInt32be
+      dataLength <- label "Read dataLength" getInt32be
+      pure $ HeaderBlob{..}
--- test/Data/RpmDbHeaderBlobSpec.hs
+++ test/Data/RpmDbHeaderBlobSpec.hs
@@ -1,7 +1,58 @@
 module Data.RpmDbHeaderBlobSpec (spec) where
 
-import Test.Hspec
+import Data.ByteString.Lazy qualified as BLS
+import Data.List (isSuffixOf)
+import Data.Rpm.DbHeaderBlob (headerBlobInit, HeaderBlob (..))
+import Test.Hspec (
+  Spec,
+  describe,
+  fcontext,
+  it,
+  runIO,
+  shouldSatisfy, context, shouldBe,
+ )
 
 spec :: Spec
-spec = pure ()
+spec = fcontext "" $ do
+  testBlob <- runIO $ BLS.readFile "test/Data/test_data/pkg_blob.bin"
+  headerBlobErrSpec
+  headerBlobSpec testBlob
 
+headerBlobSpec :: BLS.ByteString -> Spec
+headerBlobSpec bs = describe "header blob parsing" $ do
+  context "minimal example" $ do
+    it "Parses data length and index length" $ do
+      let testBS = BLS.pack [2,0,0,1 -- indexLen
+                            ,3,0,0,2] -- dataLen
+      let fromBigEndianBlob = HeaderBlob {
+            indexLength  = 0x02000001
+            , dataLength = 0x03000002
+                                         }
+      headerBlobInit testBS `shouldBe` Right fromBigEndianBlob
+
+  context "Real example " $ do
+    it "Parses data length and index length" $ do
+      let expected = HeaderBlob {
+            indexLength  = 0x00000050
+            , dataLength = 0x00008ebc
+            }
+      headerBlobInit bs `shouldBe` Right expected
+  
+
+headerBlobErrSpec :: Spec
+headerBlobErrSpec =
+  describe "headerBlob parsing errors" $ do
+    it "Should report failure when parsing nonexistent index length" $
+      headerBlobInit ""
+        `shouldSatisfy` ( \case
+                            Left ("", 0, err) -> "Read indexLength" `isSuffixOf` err
+                            _ -> False
+                        )
+
+    let invalidDl = BLS.pack [0, 0, 0, 1]
+    it "Should report failure when parsing nonexistent data length" $
+      headerBlobInit invalidDl
+        `shouldSatisfy` ( \case
+                            Left ("", 4, err) -> "Read dataLength" `isSuffixOf` err
+                            _ -> False
+                        )
