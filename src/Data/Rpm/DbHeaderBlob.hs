182e2289384bfe42bb5c2b635fd0f4a6b8f78b82
Christopher Sasarak <csasarak@users.noreply.github.com>
2022-09-09 14:57:14 -0500

    Feat/rpm container sqlite ane 273 (#1044)
    * Initial sqlite database read.
    
    * Error reporting for bad package headers
    
    * Include package with Epoch
    
    * Discover packages in a sqlite database
    
    * Larger test
    
    * lint/fmt
    
    * Update changelog, cleanup.
    
    * cabal fmt
    
    * Address a PR comment, update changelog after rebase
    
    * Update src/Discovery/Walk.hs
    
    Co-authored-by: Jessica Black <me@jessica.black>
    
    * Update src/Strategy/Sqlite.hs
    
    Co-authored-by: Jessica Black <me@jessica.black>
    
    * Update src/Strategy/Sqlite.hs
    
    Co-authored-by: Jessica Black <me@jessica.black>
    
    * Fix changelog
    
    Co-authored-by: Jessica Black <me@jessica.black>

--- src/Data/Rpm/DbHeaderBlob.hs
+++ src/Data/Rpm/DbHeaderBlob.hs
@@ -2,10 +2,49 @@
 --Description: Functions for reading RPM information out of binary package headers
 module Data.Rpm.DbHeaderBlob (
   PkgInfo (..),
+  PkgConversionError (..),
   readPackageInfo,
+  pkgInfoToDependency,
 ) where
 
+import Container.OsRelease (OsInfo (..))
 import Data.Rpm.DbHeaderBlob.Internal (
   PkgInfo (..),
   readPackageInfo,
  )
+import Data.String.Conversion (ToText (toText))
+import Data.Text (Text)
+import DepTypes (
+  DepType (LinuxRPM),
+  Dependency (..),
+  VerConstraint (CEq),
+ )
+
+newtype PkgConversionError = PkgConversionError PkgInfo
+  deriving (Show)
+
+pkgInfoToDependency :: OsInfo -> PkgInfo -> Either PkgConversionError Dependency
+pkgInfoToDependency
+  (OsInfo os osVersion)
+  PkgInfo
+    { pkgName = Just pkgName
+    , pkgVersion = Just pkgVersion
+    , pkgRelease = Just pkgRelease
+    , pkgEpoch = pkgEpoch
+    , pkgArch = Just pkgArch
+    } =
+    Right $
+      Dependency
+        LinuxRPM
+        (pkgName <> "#" <> os <> "#" <> osVersion)
+        (Just version)
+        mempty
+        mempty
+        mempty
+    where
+      version :: VerConstraint
+      version = CEq $ pkgArch <> "#" <> epoch <> (pkgVersion <> "-" <> pkgRelease)
+
+      epoch :: Text
+      epoch = maybe "" ((<> ":") . toText . show) pkgEpoch
+pkgInfoToDependency _ pkg = Left $ PkgConversionError pkg
