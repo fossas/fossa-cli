a6b90a8293a25e2fa902e03c5e90a35dd2b279a9
meghfossa <86321858+meghfossa@users.noreply.github.com>
2022-09-14 12:52:45 -0600

    Includes CI environment in telemetry record (#1043)

--- src/Control/Carrier/Telemetry/Utils.hs
+++ src/Control/Carrier/Telemetry/Utils.hs
@@ -11,6 +11,7 @@ import App.Version (isDirty, versionOrBranch)
 import Control.Algebra (Has)
 import Control.Carrier.Lift (Lift, sendIO)
 import Control.Carrier.Telemetry.Types (
+  CIEnvironment (..),
   CliEnvironment (..),
   SystemInfo (SystemInfo),
   TelemetryCmdConfig (TelemetryCmdConfig),
@@ -20,8 +21,12 @@ import Control.Carrier.Telemetry.Types (
 import Control.Concurrent.STM (STM, atomically, newEmptyTMVarIO, tryReadTMVar)
 import Control.Concurrent.STM.TBMQueue (TBMQueue, newTBMQueueIO, tryReadTBMQueue)
 import Control.Monad (join, replicateM)
+import Data.Foldable (asum)
+import Data.Functor.Extra ((<$$>))
 import Data.Maybe (catMaybes)
+import Data.String.Conversion (toText)
 import Data.Text (Text)
+import Data.Text qualified as Text
 import Data.Time (diffUTCTime, getCurrentTime, nominalDiffTimeToSeconds)
 import Data.Tracing.Instrument (
   getCounterRegistry,
@@ -30,6 +35,7 @@ import Data.Tracing.Instrument (
 import Data.UUID.V4 (nextRandom)
 import GHC.Conc.Sync qualified as Conc
 import System.Args (getCommandArgs)
+import System.Environment (lookupEnv)
 import System.Info qualified as Info
 
 maxQueueSize :: Int
@@ -55,6 +61,7 @@ mkTelemetryRecord seenFatalException ctx = sendIO $ do
   cliTimedDurations <- atomically $ getItems (telTimeSpentQ ctx)
   cliUsageCounter <- atomically $ getCounterRegistry (telCounters ctx)
   finalTime <- getCurrentTime
+  ciEnv <- lookupCIEnvironment
 
   pure $
     TelemetryRecord
@@ -70,6 +77,7 @@ mkTelemetryRecord seenFatalException ctx = sendIO $ do
       , cliTotalDurationInSec = realToFrac $ nominalDiffTimeToSeconds $ diffUTCTime finalTime (telStartUtcTime ctx)
       , cliUsageCounter = cliUsageCounter
       , cliVersion = getCurrentCliVersion
+      , cliCIEnvironment = ciEnv
       }
   where
     getItems :: TBMQueue a -> STM [a]
@@ -91,3 +99,58 @@ getSystemInfo =
     Info.arch
     <$> Conc.getNumCapabilities
     <*> Conc.getNumProcessors
+
+lookupCIEnvironment :: IO (Maybe CIEnvironment)
+lookupCIEnvironment = do
+  asum
+    <$> sequence
+      [ isGithubAction
+      , isAzurePipeline
+      , isBamboo
+      , isSomeCI
+      ]
+  where
+    -- Ref: https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables
+    isGithubAction :: IO (Maybe CIEnvironment)
+    isGithubAction = do
+      isGH <- isTrue "GITHUB_ACTIONS"
+      pure $ if isGH then Just GithubAction else Nothing
+
+    -- Ref: https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&viewFallbackFrom=vsts&tabs=yaml#system-variables
+    isAzurePipeline :: IO (Maybe CIEnvironment)
+    isAzurePipeline = do
+      isAzure <- isTrue "TF_BUILD"
+      pure $ if isAzure then Just AzurePipeline else Nothing
+
+    -- Ref: https://confluence.atlassian.com/bamboo/bamboo-variables-289277087.html#Bamboovariables-Build-specificvariables
+    isBamboo :: IO (Maybe CIEnvironment)
+    isBamboo = do
+      isB <- hasSomeValue "bamboo.buildKey"
+      pure $ if isB then Just Bamboo else Nothing
+
+    -- Catch all for CI systems, typically they set either CI or BUILD_ID environment
+    -- variable. Example: Jenkins, GitlabCI, etc.
+    isSomeCI :: IO (Maybe CIEnvironment)
+    isSomeCI = do
+      hasCIEnv <- hasSomeValue "CI"
+      hasBuildIdEnv <- hasSomeValue "BUILD_ID"
+      pure $ if (hasCIEnv || hasBuildIdEnv) then Just UnknownCI else Nothing
+
+    lookupName :: String -> IO (Maybe Text)
+    lookupName name = toText <$$> lookupEnv name
+
+    -- True if environment variable has non empty value, otherwise False.
+    hasSomeValue :: String -> IO Bool
+    hasSomeValue name = do
+      value <- lookupName name
+      pure $ case value of
+        Nothing -> False
+        Just txt -> not $ Text.null txt
+
+    -- True if environment variable represents 'true' boolean value, otherwise False.
+    isTrue :: String -> IO Bool
+    isTrue name = do
+      value <- lookupName name
+      pure $ case value of
+        Nothing -> False
+        Just txt -> Text.toLower txt == "true"
