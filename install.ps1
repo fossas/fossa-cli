f5428228520a9dc1f6384900bb8bd870dc861579
spatten <scott@fossa.com>
2022-09-19 16:59:18 -0700

    fix the v1 installer for windows

--- install.ps1
+++ install.ps1
@@ -11,8 +11,8 @@ Param()
 $OldEAP = $ErrorActionPreference #Preserve the original value
 $ErrorActionPreference = "Stop"
 $release_v1_upperbound="v1.1.10"
+$releaseVersionSemver = $release_v1_upperbound.TrimStart("v");
 $github = "https://github.com"
-$latestUri = "$github/fossas/fossa-cli/releases/$release_v1_upperbound"
 $extractDir = "$env:ALLUSERSPROFILE\fossa-cli"
 
 Write-host "`n"
@@ -21,12 +21,12 @@ Write-Host "Deprecation Warning"
 Write-Host "-------------------"
 Write-host "`n"
 Write-Host "You are installing FOSSA CLI v1, which is no longer in active"
-Write-Host "development. FOSSA will not address new defects found in CLI v1."  
+Write-Host "development. FOSSA will not address new defects found in CLI v1."
 Write-host "`n"
 Write-Host "Please upgrade to the latest FOSSA CLI."
 Write-host "`n"
 Write-Host "Please upgrade to FOSSA CLI v3 by using install-latest script:"
-Write-Host "--------------------------------------------------------------" 
+Write-Host "--------------------------------------------------------------"
 Write-Host "    Set-ExecutionPolicy Bypass -Scope Process -Force; iex  ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/fossas/fossa-cli/master/install-latest.ps1'))"
 Write-host "`n"
 Write-Host "Migration guide for FOSSA CLI v3:"
@@ -34,18 +34,10 @@ Write-Host "---------------------------------"
 Write-Host "    https://github.com/fossas/fossa-cli/blob/master/docs/differences-from-v1.md#how-to-upgrade-to-fossa-3x"
 Write-host "`n"
 
-Write-Verbose "Looking up latest v1 release..."
-
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
-$releasePage = Invoke-RestMethod $latestUri
-
-if ($releasePage -inotmatch 'href=\"(.*?releases\/download\/.*?windows.*?)\"')
-{
-    throw "Did not find latest Windows release at $latestUri"
-}
-
-$downloadUri = "$github/$($Matches[1])"
+$downloadPath = "fossas/fossa-cli/releases/download/$($release_v1_upperbound)/fossa-cli_$($releaseVersionSemver)_windows_amd64.zip"
+$downloadUri = "$github/$downloadPath"
 Write-Verbose "Downloading from: $downloadUri"
 
 $TempDir = Join-Path ([System.IO.Path]::GetTempPath()) "fossa-cli"
