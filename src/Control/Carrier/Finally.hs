bf38825c6831f48f536f807df7b954025756fba0
Leo Zhang <leo@fossa.io>
2022-11-15 22:19:05 -0800

    doc: Document internals of Finally

--- src/Control/Carrier/Finally.hs
+++ src/Control/Carrier/Finally.hs
@@ -21,18 +21,30 @@ import Data.Functor (void)
 import Data.IORef
 import Prelude
 
+-- | `FinallyC` implements `Finally` by carrying around an `IORef` containing a
+-- list of finalizer actions.
 newtype FinallyC m a = FinallyC {runFinallyC :: ReaderC (IORef [FinallyC m ()]) m a}
   deriving (Functor, Applicative, Monad, MonadIO)
 
+-- | `runFinally` interprets a `FinallyC` by running the underlying effect
+--  stack, then using `finally` to run all registered finalizers.
 runFinally :: Has (Lift IO) sig m => FinallyC m a -> m a
 runFinally (FinallyC go) = do
   ref <- sendIO $ newIORef []
+  -- Note that we `traverse_ runFinally` because finalizers run in the full
+  -- effect stack (including `Finally`), and can possibly register their own
+  -- finalizers.
   runReader ref go `finally` (traverse_ runFinally =<< sendIO (readIORef ref))
 
 instance (Has (Lift IO) sig m, Algebra sig m) => Algebra (Finally :+: sig) (FinallyC m) where
   alg hdl sig ctx = FinallyC $ case sig of
+    -- Register a new finalizer `OnExit` by grabbing the IORef from the Reader
+    -- and cons'ing the new finalizer.
     L (OnExit go) -> do
       ref <- ask
+      -- Note that we re-wrap the finalizer using `hdl` so that finalizers can
+      -- run in the full effect stack.
       sendIO $ atomicModifyIORef' ref (\cur -> (void (hdl (go <$ ctx)) : cur, ()))
       pure ctx
+    -- Run `FinallyC` into a `ReaderC`, and run the rest of the effect stack.
     R other -> alg (runFinallyC . hdl) (R other) ctx
