3b07ab8b9cfa237b184c4a6450dc851a022b908b
Christopher Sasarak <chris@fossa.com>
2023-04-11 17:21:55 -0500

    Revert split up EmbeddedBinary

--- src/App/Fossa/EmbeddedBinary/Wiggins.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-
-module App.Fossa.EmbeddedBinary.Wiggins (embeddedBinaryWiggins) where
-
-import Data.ByteString (ByteString)
-import Data.FileEmbed.Extra (embedFileIfExists)
-
-embeddedBinaryWiggins :: ByteString
-embeddedBinaryWiggins = $(embedFileIfExists "vendor-bins/wiggins")
