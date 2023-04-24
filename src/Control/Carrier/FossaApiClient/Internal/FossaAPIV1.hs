475c94dc7ac9dbf0fe82413062d8fe49588b951b
spatten <scott@fossa.com>
2023-04-24 14:30:38 -0700

    clarify how the archive upload endpoint uses the ArchiveComponent record

--- src/Control/Carrier/FossaApiClient/Internal/FossaAPIV1.hs
+++ src/Control/Carrier/FossaApiClient/Internal/FossaAPIV1.hs
@@ -807,6 +807,7 @@ archiveBuildUpload apiOpts archive = runEmpty $
     let opts = "dependency" =: True <> "rawLicenseScan" =: True
     -- The API route expects an array of archives, but doesn't properly handle multiple archives so we upload
     -- an array of a single archive.
+    -- forceRebuild and fullFiles are ignored by this endpoint. They are only used by the licenseScanFinalize endpoint.
     let archiveProjects = ArchiveComponents [archive] False $ FullFileUploads False
     -- The response appears to either be "Created" for new builds, or an error message for existing builds.
     -- Making the actual return value of "Created" essentially worthless.
