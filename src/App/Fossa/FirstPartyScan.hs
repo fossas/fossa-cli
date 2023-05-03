eda6752c88e3cd52904fead35f095296e552659e
spatten <scott@fossa.com>
2023-05-03 16:12:09 -0700

    run the scans if the flags say to

--- src/App/Fossa/FirstPartyScan.hs
+++ src/App/Fossa/FirstPartyScan.hs
@@ -1,56 +1,51 @@
 module App.Fossa.FirstPartyScan (
   runFirstPartyScan,
 ) where
-import Srclib.Types (SourceUnit(..))
-import Data.List.NonEmpty (NonEmpty)
-import App.Types (FirstPartyScansFlag (..))
+import Srclib.Types (LicenseSourceUnit)
+import App.Types (FirstPartyScansFlag (..), FullFileUploads (FullFileUploads))
 import Fossa.API.Types (ApiOpts(..), Organization (..))
-import Control.Effect.Diagnostics (Diagnostics, context, fatalText)
+import Control.Effect.Diagnostics (Diagnostics)
 import Control.Effect.FossaApiClient (FossaApiClient, getOrganization)
-import Control.Effect.Lift (Has, Lift)
+import Control.Effect.Lift (Lift)
 import Control.Effect.StickyLogger (StickyLogger)
 import Effect.Exec (Exec)
-import Effect.Logger (Logger)
-import Effect.ReadFS (ReadFS, doesFileExist, readContentsJson, readContentsYaml)
-import Path (Abs, Dir, File, Path, mkRelFile, (</>))
 import Control.Carrier.FossaApiClient (runFossaApiClient)
+import App.Fossa.ManualDeps (VendoredDependency(..))
+import App.Fossa.LicenseScanner (scanVendoredDep)
+import Effect.ReadFS (ReadFS, Has)
+import Path (Abs, Path, Dir)
 
 runFirstPartyScan ::
   ( Has Diagnostics sig m
-  , Has ReadFS sig m
   , Has (Lift IO) sig m
   , Has StickyLogger sig m
-  , Has Logger sig m
   , Has Exec sig m
+  , Has ReadFS sig m
   ) =>
   Path Abs Dir ->
   Maybe ApiOpts ->
   FirstPartyScansFlag ->
-  m (Maybe ([]SourceUnit))
+  m (Maybe LicenseSourceUnit)
 runFirstPartyScan root maybeApiOpts firstPartyScanFlag = do
   -- if we do not have api opts, then we act as if the org defaults to not running first-party scans
   case maybeApiOpts of
-    Nothing -> firstPartyScanMain root $ shouldRunFirstPartyScans firstPartyScanFlag False
+    Nothing -> firstPartyScanMaybe root firstPartyScanFlag False
     Just apiOpts -> runFossaApiClient apiOpts $ firstPartyScanWithOrgInfo root firstPartyScanFlag
 
 firstPartyScanWithOrgInfo ::
   ( Has Diagnostics sig m
-  , Has ReadFS sig m
   , Has (Lift IO) sig m
   , Has StickyLogger sig m
-  , Has Logger sig m
   , Has Exec sig m
+  , Has ReadFS sig m
   , Has FossaApiClient sig m
   ) =>
   Path Abs Dir ->
   FirstPartyScansFlag ->
-  m (Maybe ([]SourceUnit))
-firstPartyScanWithOrgInfo root firstPartyScanFlags = do
+  m (Maybe LicenseSourceUnit)
+firstPartyScanWithOrgInfo root firstPartyScanFlag = do
   org <- getOrganization
-  let runFirstPartyScans = shouldRunFirstPartyScans firstPartyScanFlags $ orgDefaultsToFirstPartyScans org
-  firstPartyScanMain root runFirstPartyScans
-
-
+  firstPartyScanMaybe root firstPartyScanFlag $ orgDefaultsToFirstPartyScans org
 
 shouldRunFirstPartyScans :: FirstPartyScansFlag -> Bool -> Bool
 shouldRunFirstPartyScans firstPartyScansFlag orgDefaultsToFirstParty =
@@ -59,16 +54,33 @@ shouldRunFirstPartyScans firstPartyScansFlag orgDefaultsToFirstParty =
     (FirstPartyScansOffFromFlag, _) -> False
     (FirstPartyScansUseDefault, orgDefault) -> orgDefault
 
-firstPartyScanMain ::
+firstPartyScanMaybe ::
   ( Has Diagnostics sig m
-  , Has ReadFS sig m
   , Has (Lift IO) sig m
   , Has StickyLogger sig m
-  , Has Logger sig m
   , Has Exec sig m
+  , Has ReadFS sig m
   ) =>
   Path Abs Dir ->
+  FirstPartyScansFlag ->
   Bool ->
-  m (Maybe ([]SourceUnit))
-firstPartyScanMain base runFirstPartyScans =
-  pure Nothing
+  m (Maybe LicenseSourceUnit)
+firstPartyScanMaybe base firstPartyScansFlag orgDefaultsToFirstParty = do
+  let runFirstPartyScans = shouldRunFirstPartyScans firstPartyScansFlag orgDefaultsToFirstParty
+  case runFirstPartyScans of
+    (True) -> Just <$> firstPartyScanMain base
+    (False) -> pure Nothing
+
+
+firstPartyScanMain ::
+  ( Has Diagnostics sig m
+  , Has (Lift IO) sig m
+  , Has StickyLogger sig m
+  , Has Exec sig m
+  , Has ReadFS sig m
+  ) =>
+  Path Abs Dir ->
+  m LicenseSourceUnit
+firstPartyScanMain base = do
+  let vdep = VendoredDependency "first-party" "." Nothing
+  scanVendoredDep base Nothing ( FullFileUploads False ) vdep
