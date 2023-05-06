a873f347d9b75460107a6e04bad84f6e3d1874c4
spatten <scott@fossa.com>
2023-05-05 18:01:48 -0700

    more formatting

--- test/App/Fossa/FirstPartyScanSpec.hs
+++ test/App/Fossa/FirstPartyScanSpec.hs
@@ -5,15 +5,15 @@ module App.Fossa.FirstPartyScanSpec (spec) where
 import App.Fossa.FirstPartyScan (firstPartyScanWithOrgInfo)
 import App.Types (FirstPartyScansFlag (..))
 import Control.Algebra (Has)
+import Control.Effect.FossaApiClient (FossaApiClientF (..))
+import Fossa.API.Types (Organization (..))
 import Path (Dir, Path, Rel, mkRelDir, (</>))
 import Path.IO (getCurrentDir)
-import Test.Effect (it', shouldBe', expectationFailure', expectFatal')
+import Srclib.Types (LicenseSourceUnit (..))
+import Test.Effect (expectFatal', expectationFailure', it', shouldBe')
 import Test.Fixtures qualified as Fixtures
 import Test.Hspec (Spec, describe, runIO)
 import Test.MockApi (MockApi, alwaysReturns)
-import Fossa.API.Types (Organization(..))
-import Control.Effect.FossaApiClient (FossaApiClientF(..))
-import Srclib.Types (LicenseSourceUnit(..))
 
 fixtureDir :: Path Rel Dir
 fixtureDir = $(mkRelDir "test/App/Fossa/VendoredDependency/testdata/repo")
@@ -43,20 +43,19 @@ spec = do
       licenseSourceUnit <- firstPartyScanWithOrgInfo scanDir FirstPartyScansOnFromFlag
       case licenseSourceUnit of
         Nothing -> expectationFailure' "first party scan should have run"
-        Just LicenseSourceUnit{ licenseSourceUnitLicenseUnits = units} -> length units `shouldBe'` 2
+        Just LicenseSourceUnit{licenseSourceUnitLicenseUnits = units} -> length units `shouldBe'` 2
 
     it' "should run if the organization defaults to running first-party scans and is not forced on" $ do
       expectGetOrganizationThatDefaultsToFirstPartyScans
       licenseSourceUnit <- firstPartyScanWithOrgInfo scanDir FirstPartyScansUseDefault
       case licenseSourceUnit of
         Nothing -> expectationFailure' "first party scan should have run"
-        Just LicenseSourceUnit{ licenseSourceUnitLicenseUnits = units} -> length units `shouldBe'` 2
+        Just LicenseSourceUnit{licenseSourceUnitLicenseUnits = units} -> length units `shouldBe'` 2
 
     it' "should not run if the organization defaults to running first-party scans and is forced off" $ do
       expectGetOrganizationThatDefaultsToFirstPartyScans
       licenseSourceUnit <- firstPartyScanWithOrgInfo scanDir FirstPartyScansOffFromFlag
       licenseSourceUnit `shouldBe'` Nothing
-
 
 -- The default org defaults to not running first party scans but has first-party scans enabled
 expectGetOrganizationThatDefaultsToNoFirstPartyScans :: Has MockApi sig m => m ()
