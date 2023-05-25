ba30869d9e4463999457a6629a48e34f123d19cd
spatten <scott@fossa.com>
2023-05-25 11:09:41 -0700

    no need to recreate mergeSourceAndLicenseUnits anymore

--- src/App/Fossa/Analyze.hs
+++ src/App/Fossa/Analyze.hs
@@ -32,7 +32,7 @@ import App.Fossa.Analyze.Types (
   DiscoveredProjectIdentifier (..),
   DiscoveredProjectScan (..),
  )
-import App.Fossa.Analyze.Upload (uploadSuccessfulAnalysis)
+import App.Fossa.Analyze.Upload (uploadSuccessfulAnalysis, mergeSourceAndLicenseUnits)
 import App.Fossa.BinaryDeps (analyzeBinaryDeps)
 import App.Fossa.Config.Analyze (
   AnalyzeCliOpts,
@@ -120,7 +120,7 @@ import Prettyprinter.Render.Terminal (
   color,
  )
 import Srclib.Converter qualified as Srclib
-import Srclib.Types (LicenseSourceUnit (licenseSourceUnitLicenseUnits), Locator, SourceUnit, licenseUnitToFullSourceUnit, sourceUnitToFullSourceUnit)
+import Srclib.Types (LicenseSourceUnit, Locator, SourceUnit, sourceUnitToFullSourceUnit)
 import Types (DiscoveredProject (..), FoundTargets)
 
 debugBundlePath :: FilePath
@@ -471,10 +471,7 @@ buildResult includeAll srcUnits projects firstPartyScanResults =
     mergedUnits = case firstPartyScanResults of
       Nothing -> map sourceUnitToFullSourceUnit finalSourceUnits
       Just licenseUnits -> do
-        fromLicenseUnits ++ fromSourceUnits
-        where
-          fromSourceUnits = map sourceUnitToFullSourceUnit finalSourceUnits
-          fromLicenseUnits = NE.toList . NE.map licenseUnitToFullSourceUnit $ licenseSourceUnitLicenseUnits licenseUnits
+        NE.toList $ mergeSourceAndLicenseUnits finalSourceUnits licenseUnits
     finalSourceUnits = srcUnits ++ scannedUnits
     scannedUnits = map (Srclib.toSourceUnit (fromFlag IncludeAll includeAll)) projects
 
