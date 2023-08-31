b5b93712f21cb5e66909ea72795f1b6ccd4237e7
Zachary LaVallee <zcjlavallee@gmail.com>
2023-08-31 09:41:04 -0700

    fix whitespace

--- test/Maven/PomStrategySpec.hs
+++ test/Maven/PomStrategySpec.hs
@@ -25,7 +25,7 @@ spec = do
 
     it "should interpolate multiple properties" $ do
       interpolateProperties pom "${project.groupId}${project.artifactId}" `shouldBe` "MYGROUPMYARTIFACT"
-    
+
     it "should not infinitely recurse when interpolating a property that is interpolated to itself" $ do
       let pom' = pom{pomProperties = Map.singleton "project.groupId" "${project.groupId"}
       interpolateProperties pom' "${project.groupId}" `shouldBe` "project.groupId"
