55e81e062897a55483b4a6f7e101c8aa63cf8401
spatten <scott@fossa.com>
2023-11-01 13:36:31 -0700

    download themis and its index from fossas/themis instead of fossas/basis

--- vendor_download.sh
+++ vendor_download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Requires environment variables:
-#   GITHUB_TOKEN    A token with access to the fossas/basis repository
+#   GITHUB_TOKEN    A token with access to the fossas/themis repository
 #
 # Requires binary dependencies in $PATH:
 #   jq              Parse and manipulate json structures.
@@ -31,13 +31,12 @@ rm -f vendor-bins/*
 mkdir -p vendor-bins
 
 ASSET_POSTFIX=""
-BASIS_ASSET_POSTFIX=""
+THEMIS_ASSET_POSTFIX=""
 LERNIE_ASSET_POSTFIX=""
-OS_WINDOWS=false
 case "$(uname -s)" in
   Darwin)
     ASSET_POSTFIX="darwin"
-    BASIS_ASSET_POSTFIX="darwin-amd64"
+    THEMIS_ASSET_POSTFIX="darwin-amd64"
     case "$(uname -m)" in
       arm64)
         LERNIE_ASSET_POSTFIX="aarch64-macos"
@@ -51,22 +50,20 @@ case "$(uname -s)" in
 
   Linux)
     ASSET_POSTFIX="linux"
-    BASIS_ASSET_POSTFIX="linux-amd64"
+    THEMIS_ASSET_POSTFIX="linux-amd64"
     LERNIE_ASSET_POSTFIX="x86_64-linux"
     ;;
 
   *)
     echo "Warn: Assuming $(uname -s) is Windows"
     ASSET_POSTFIX="windows.exe"
-    BASIS_ASSET_POSTFIX="windows-amd64"
+    THEMIS_ASSET_POSTFIX="windows-amd64"
     LERNIE_ASSET_POSTFIX="x86_64-windows.exe"
-    OS_WINDOWS=true
     ;;
 esac
 
 # Download latest release of Themis and its index
 
-TAG="latest"
 echo "Downloading asset information from latest tag for architecture '$ASSET_POSTFIX'"
 
 echo "Downloading themis binary from latest release"
@@ -74,15 +71,15 @@ THEMIS_RELEASE_JSON=vendor-bins/themis-release.json
 curl -sSL \
     -H "Authorization: token $GITHUB_TOKEN" \
     -H "Accept: application/vnd.github.v3.raw" \
-    https://api.github.com/repos/fossas/basis/releases/latest > $THEMIS_RELEASE_JSON
+    https://api.github.com/repos/fossas/themis/releases/latest > $THEMIS_RELEASE_JSON
 
 THEMIS_TAG=$(jq -cr ".name" $THEMIS_RELEASE_JSON)
 echo "Using themis release: $THEMIS_TAG"
-FILTER=".name == \"themis-cli-$BASIS_ASSET_POSTFIX\""
+FILTER=".name == \"themis-cli-$THEMIS_ASSET_POSTFIX\""
 jq -c ".assets | map({url: .url, name: .name}) | map(select($FILTER)) | .[]" $THEMIS_RELEASE_JSON | while read ASSET; do
   URL="$(echo $ASSET | jq -c -r '.url')"
   NAME="$(echo $ASSET | jq -c -r '.name')"
-  OUTPUT="$(echo vendor-bins/$NAME | sed 's/-'$BASIS_ASSET_POSTFIX'$//')"
+  OUTPUT="$(echo vendor-bins/$NAME | sed 's/-'$THEMIS_ASSET_POSTFIX'$//')"
 
   echo "Downloading '$NAME' to '$OUTPUT'"
   curl -sL -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/octet-stream" -s $URL > $OUTPUT
