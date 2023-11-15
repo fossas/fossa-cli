bf2d81a964932d9fa4723740b1552b946145269b
Megh <megh@fossa.com>
2023-11-14 19:23:06 -0700

    fix tests

--- test/Test/Fixtures.hs
+++ test/Test/Fixtures.hs
@@ -389,6 +389,7 @@ experimentalConfig =
   ExperimentalAnalyzeConfig
     { allowedGradleConfigs = Nothing
     , useV3GoResolver = GoModulesBasedTactic
+    , analyzePathDependencies = False
     }
 
 vendoredDepsOptions :: VendoredDependencyOptions
