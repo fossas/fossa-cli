7d41f3aa0b61d0ffb73aa5d6ace6fe4f685d0707
meghfossa <megh@fossa.com>
2024-02-10 07:38:18 -0700

    fmting

--- src/App/Fossa/Analyze.hs
+++ src/App/Fossa/Analyze.hs
@@ -101,7 +101,7 @@ import Data.List.NonEmpty qualified as NE
 import Data.Maybe (fromMaybe, mapMaybe)
 import Data.String.Conversion (decodeUtf8, toText)
 import Data.Text.Extra (showT)
-import Diag.Result (resultToMaybe, Result (..))
+import Diag.Result (Result (..), resultToMaybe)
 import Discovery.Archive qualified as Archive
 import Discovery.Filters (AllFilters, MavenScopeFilters, applyFilters, filterIsVSIOnly, ignoredPaths, isDefaultNonProductionPath)
 import Discovery.Projects (withDiscoveredProjects)
