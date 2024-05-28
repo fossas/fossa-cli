3db1aee253cf9f8b4c36e00cb5175b4fb1a900b9
spatten <scott@fossa.com>
2024-05-27 19:12:03 -0700

    cleanup

--- src/App/Fossa/SBOM.hs
+++ src/App/Fossa/SBOM.hs
@@ -3,7 +3,6 @@ module App.Fossa.SBOM (
 ) where
 
 import App.Fossa.Config.SBOM (
-  SBOMAnalyzeConfig (..),
   SBOMCommand,
   SBOMScanConfig (..),
  )
@@ -11,32 +10,24 @@ import App.Fossa.Config.SBOM qualified as Config
 import App.Fossa.SBOM.Analyze qualified as Analyze
 import App.Fossa.SBOM.Test qualified as Test
 import App.Fossa.Subcommand (SubCommand)
-import App.Support (supportUrl)
-import Control.Carrier.Debug (Debug, ignoreDebug)
-import Control.Carrier.FossaApiClient (runFossaApiClient)
-import Control.Carrier.StickyLogger (StickyLogger)
+import Control.Carrier.Debug (ignoreDebug)
 import Control.Effect.Diagnostics (
   Diagnostics,
   Has,
  )
 import Control.Effect.Lift (Lift)
-import Control.Effect.Telemetry (Telemetry)
 import Control.Monad (void)
-import Effect.Exec (Exec)
 import Effect.Logger (
   Logger,
  )
-import Effect.ReadFS (ReadFS)
 
 sbomSubCommand :: SubCommand SBOMCommand SBOMScanConfig
 sbomSubCommand = Config.mkSubCommand dispatch
 
 dispatch ::
   ( Has Diagnostics sig m
-  , Has Exec sig m
   , Has (Lift IO) sig m
   , Has Logger sig m
-  , Has ReadFS sig m
   ) =>
   SBOMScanConfig ->
   m ()
