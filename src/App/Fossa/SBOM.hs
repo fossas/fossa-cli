a6755b5d7c8c288a1dc4a684f43297a2f666b35e
spatten <scott@fossa.com>
2024-05-28 15:33:48 -0700

    make --debug flag do something

--- src/App/Fossa/SBOM.hs
+++ src/App/Fossa/SBOM.hs
@@ -2,6 +2,7 @@ module App.Fossa.SBOM (
   sbomSubCommand,
 ) where
 
+import App.Fossa.Analyze.Debug (collectDebugBundle)
 import App.Fossa.Config.SBOM (
   SBOMCommand,
   SBOMScanConfig (..),
@@ -10,29 +11,45 @@ import App.Fossa.Config.SBOM qualified as Config
 import App.Fossa.SBOM.Analyze qualified as Analyze
 import App.Fossa.SBOM.Test qualified as Test
 import App.Fossa.Subcommand (SubCommand)
+import Codec.Compression.GZip qualified as GZip
 import Control.Carrier.Debug (ignoreDebug)
+import Control.Carrier.Diagnostics qualified as Diag
 import Control.Effect.Diagnostics (
   Diagnostics,
   Has,
  )
-import Control.Effect.Lift (Lift)
-import Control.Monad (void)
+import Control.Effect.Lift (Lift, sendIO)
+import Data.Aeson qualified as Aeson
+import Data.ByteString.Lazy qualified as BL
+import Effect.Exec (Exec)
 import Effect.Logger (
   Logger,
+  Severity (..),
  )
+import Effect.ReadFS (ReadFS)
 
 sbomSubCommand :: SubCommand SBOMCommand SBOMScanConfig
 sbomSubCommand = Config.mkSubCommand dispatch
+
+debugBundlePath :: FilePath
+debugBundlePath = "fossa.debug.json.gz"
 
 dispatch ::
   ( Has Diagnostics sig m
   , Has (Lift IO) sig m
   , Has Logger sig m
+  , Has ReadFS sig m
+  , Has Exec sig m
   ) =>
   SBOMScanConfig ->
   m ()
 dispatch = \case
-  -- TODO: deal with logging properly
   -- TODO: Add telemetry
-  AnalyzeCfg cfg -> void $ ignoreDebug $ Analyze.analyze cfg
+  AnalyzeCfg cfg -> do
+    case Config.severity cfg of
+      SevDebug -> do
+        (scope, res) <- collectDebugBundle cfg $ Diag.errorBoundaryIO $ Analyze.analyze cfg
+        sendIO . BL.writeFile debugBundlePath . GZip.compress $ Aeson.encode scope
+        Diag.rethrow res
+      _ -> ignoreDebug $ Analyze.analyze cfg
   TestCfg cfg -> Test.test cfg
