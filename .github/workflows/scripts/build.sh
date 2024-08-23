e0b78451bea53964efdb282c0c44dd524f9c59e1
Christopher Sasarak <chris@fossa.com>
2024-08-23 15:11:22 -0500

    Forget target for now, try build.

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-cabal update && cabal build --project-file=cabal.project.ci.linux
+set -e
+
+make build-test-data
+cabal update
+cabal build --project-file=cabal.project.ci.linux all
+cabal test --project-file=cabal.project.ci.linux
