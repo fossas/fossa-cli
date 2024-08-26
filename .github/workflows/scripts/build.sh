6776237455581417aac538f0eb8824452b6b9585
Christopher Sasarak <chris@fossa.com>
2024-08-26 16:47:39 -0500

    Run git config update just before build.

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -13,13 +13,16 @@ PROJECT_FILE=$2
 
 make build-test-data
 
+# Does this need to run in the container?
+# It normally runs outside.
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
 # With dist-newstyle caches:
 # Cabal mainly knows to recompile based on changes to files.
 # Tagging in git doesn't reliably change a file in a fixed location that cabal/GHC can track to indicate that there's a new tag.
 # For our release process, we merge to master, which builds (and may store a dist-newstyle cache), then push a release tag.
 # During the tag build, cabal/GHC may not realize that they have to rebuild the Version.hs file because the tag is invisible to it.
 # This line adds a comment to our version source file to prompt cabal/GHC to rebuild Version.hs.
-
 echo "{- $GITHUB_RUN_ID -}" >> src/App/Version.hs
 cabal update
 cabal build --project-file=cabal.project.ci.linux all
