ec81b5bf2b68452c4b5680c2ade7e84a23a1d365
Christopher Sasarak <chris@fossa.com>
2024-08-27 11:31:30 -0500

    Try moving rust build into script.

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -4,12 +4,26 @@ set -e
 set -x
 
 if [ $# -lt 2 ] ; then
-    echo "Usage: ./compute_cache_key <RUNNER OS> <PROJECT FILE>"
+    echo "Usage: ./compute_cache_key <RUNNER OS> <PROJECT FILE>" 
     exit 1
 fi
 
 RUNNER_OS=$1
 PROJECT_FILE=$2
+
+# Install rust tooling
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal
+cargo install nextest
+
+rustc -V
+cargo -V
+
+# Build Rust binaries
+cargo build --release
+
+cargo nextest run --release
+
+# Now do Haskell things
 
 make build-test-data
 
