3aa38df2e4829ab8219352966732774c07d8c113
Nic Ficca <nic@fossa.com>
2025-10-28 12:44:55 -0400

    Extract ficus result source unit

--- src/App/Fossa/Analyze.hs
+++ src/App/Fossa/Analyze.hs
@@ -51,6 +51,7 @@ import App.Fossa.Config.Analyze (
 import App.Fossa.Config.Analyze qualified as Config
 import App.Fossa.Config.Common (DestinationMeta (..), destinationApiOpts, destinationMetadata)
 import App.Fossa.Ficus.Analyze (analyzeWithFicus)
+import App.Fossa.Ficus.Types (FicusAnalysisResults (vendoredDependencyScanResults), FicusVendoredDependencyScanResults (FicusVendoredDependencyScanResults))
 import App.Fossa.FirstPartyScan (runFirstPartyScan)
 import App.Fossa.Lernie.Analyze (analyzeWithLernie)
 import App.Fossa.Lernie.Types (LernieResults (..))
@@ -103,7 +104,7 @@ import Data.Flag (Flag, fromFlag)
 import Data.Foldable (traverse_)
 import Data.Functor (($>))
 import Data.List.NonEmpty qualified as NE
-import Data.Maybe (fromMaybe, isJust, mapMaybe)
+import Data.Maybe (fromMaybe, isJust, mapMaybe, maybeToList)
 import Data.String.Conversion (decodeUtf8, toText)
 import Data.Text.Extra (showT)
 import Data.Traversable (for)
@@ -378,13 +379,22 @@ analyze cfg = Diag.context "fossa-analyze" $ do
       vsiResults' :: [SourceUnit]
       vsiResults' = fromMaybe [] $ join (resultToMaybe vsiResults)
 
+      ficusResults' :: [SourceUnit]
+      ficusResults' =
+        maybeToList $
+          ficusResults
+            >>= vendoredDependencyScanResults
+            >>= \(FicusVendoredDependencyScanResults maybeSrcUnit) -> maybeSrcUnit
+
       additionalSourceUnits :: [SourceUnit]
-      additionalSourceUnits = vsiResults' <> mapMaybe (join . resultToMaybe) [manualSrcUnits, binarySearchResults, dynamicLinkedResults]
+      additionalSourceUnits = vsiResults' <> ficusResults' <> mapMaybe (join . resultToMaybe) [manualSrcUnits, binarySearchResults, dynamicLinkedResults]
   traverse_ (Diag.flushLogs SevError SevDebug) [manualSrcUnits, binarySearchResults, dynamicLinkedResults]
   -- Flush logs using the original Result from VSI.
   traverse_ (Diag.flushLogs SevError SevDebug) [vsiResults]
   -- Flush logs from lernie
   traverse_ (Diag.flushLogs SevError SevDebug) [maybeLernieResults]
+  -- Flush logs from ficus
+  traverse_ (Diag.flushLogs SevError SevDebug) [maybeFicusResults]
 
   maybeFirstPartyScanResults <-
     Diag.errorBoundaryIO . diagToDebug $
