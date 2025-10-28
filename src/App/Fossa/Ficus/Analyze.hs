4110f743b5c066949a16043c4ae377431f034111
Nic Ficca <nic@fossa.com>
2025-10-28 13:41:05 -0400

    Use proper metadata format

--- src/App/Fossa/Ficus/Analyze.hs
+++ src/App/Fossa/Ficus/Analyze.hs
@@ -185,7 +185,14 @@ vendoredDepsToSourceUnit deps =
       SourceUnitDependency
         { sourceDepLocator = vendoredDepToLocator dep
         , sourceDepImports = []
-        , sourceDepData = Aeson.object ["path" Aeson..= ficusVendoredDependencyPath dep]
+        , sourceDepData =
+            Aeson.object
+              [ "vendored"
+                  Aeson..= Aeson.object
+                    [ "type" Aeson..= ("directory" :: Text)
+                    , "path" Aeson..= ficusVendoredDependencyPath dep
+                    ]
+              ]
         }
 
 runFicus ::
