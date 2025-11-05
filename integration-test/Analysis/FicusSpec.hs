460a08d47bc32e1dc02afb6c4283b0e7bb2dfaeb
spatten <scott@fossa.com>
2025-11-05 14:37:01 -0800

    fix call to analyzeWithFicus in a test

--- integration-test/Analysis/FicusSpec.hs
+++ integration-test/Analysis/FicusSpec.hs
@@ -51,7 +51,7 @@ spec = do
       testDataExists <- PIO.doesDirExist testDataDir
       testDataExists `shouldBe` True
 
-      result <- runStack . runDiagnostics . ignoreStickyLogger . ignoreLogger . runExecIO . runReadFSIO $ analyzeWithFicus testDataDir apiOpts revision Nothing (Just 10) False
+      result <- runStack . runDiagnostics . ignoreStickyLogger . ignoreLogger . runExecIO . runReadFSIO $ analyzeWithFicus testDataDir apiOpts revision Nothing (Just 10) False Nothing
 
       case result of
         Success _warnings analysisResult -> do
