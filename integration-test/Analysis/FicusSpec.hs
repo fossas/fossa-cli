27a2b3cbf6375dce287bb07b5d3bf9cb9a97be09
spatten <scott@fossa.com>
2025-11-07 09:59:55 -0800

    fix a test

--- integration-test/Analysis/FicusSpec.hs
+++ integration-test/Analysis/FicusSpec.hs
@@ -56,8 +56,8 @@ spec = do
       case result of
         Success _warnings analysisResult -> do
           case analysisResult of
-            Just (FicusSnippetScanResults analysisId) -> do
-              analysisId `shouldSatisfy` (> 0)
+            Just results -> do
+              ficusSnippetScanResultsAnalysisId results `shouldSatisfy` (> 0)
             Nothing -> do
               -- No snippet scan results returned - this is acceptable for integration testing
               True `shouldBe` True
