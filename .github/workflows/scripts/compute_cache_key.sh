43cbe6cea94741ea8d9e2582e8c098abdf7b0726
Christopher Sasarak <chris@fossa.com>
2026-03-20 10:54:11 -0400

    Revert "Replace plan-hash cache keys with file-hash and tag rotation (#1666)"
    This reverts commit f190d48a755d02c54041638f911de4e26cefa12e.

---
+++ .github/workflows/scripts/compute_cache_key.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+set -e
+set -x
+
+if [ $# -lt 2 ] ; then
+    echo "Usage: ./compute_cache_key <RUNNER OS> <PROJECT FILE>"
+    exit 1
+fi
+
+RUNNER_OS=$1
+PROJECT_FILE=$2
+
+cabal --project-file="$PROJECT_FILE" update
+cabal --project-file="$PROJECT_FILE" build --dry-run
+jq '."install-plan"[]."id"' < dist-newstyle/cache/plan.json | sort > /tmp/cabal-cache-key
+echo "Install plan:"
+cat /tmp/cabal-cache-key
+
+if [ "$RUNNER_OS" = "macOS" ]; then
+   PLAN_SUM=$(shasum -a256 /tmp/cabal-cache-key)
+else
+   PLAN_SUM=$(sha256sum /tmp/cabal-cache-key)
+fi
+
+CABAL_CACHE_KEY="$(echo "$PLAN_SUM" | awk '{print $1}')"
+export CABAL_CACHE_KEY
+echo "Cabal cache key: $CABAL_CACHE_KEY"
+echo "cabal-cache-key=$CABAL_CACHE_KEY" >> "$GITHUB_OUTPUT"
+
+echo "Home dotfiles in container: $(ls -al ~/.*)"
+
+# Cleanup. Restoring this cache seems to fail if the directory already exists.
+rm -rf dist-newstyle
