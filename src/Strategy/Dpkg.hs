000913148ede761a8db4b9884550f91548c7a18d
Gaurav Bhagchandani <gaurav@fossa.com>
2026-06-15 17:09:13 -0700

    Add ability to read dbs from status.d directories

--- src/Strategy/Dpkg.hs
+++ src/Strategy/Dpkg.hs
@@ -12,13 +12,14 @@ import Data.Aeson (ToJSON)
 import Data.String.Conversion (toText)
 import Data.Text qualified as Text
 import Discovery.Filters (AllFilters)
+import Data.Foldable (find)
 import Discovery.Simple (simpleDiscover)
 import Discovery.Walk (
   WalkStep (WalkContinue),
   findFileNamed,
   walkWithFilters',
  )
-import Effect.ReadFS (Has, ReadFS)
+import Effect.ReadFS (Has, ReadFS, listDir)
 import GHC.Generics (Generic)
 import Path (Abs, Dir, File, Path, toFilePath)
 import Strategy.Dpkg.Database (analyze)
@@ -27,7 +28,6 @@ import Types (
   DiscoveredProject (..),
   DiscoveredProjectType (DpkgDatabaseProjectType),
  )
-
 data DpkgDatabase = DpkgDatabase
   { dbDir :: Path Abs Dir
   , dbFile :: Path Abs File
@@ -59,13 +59,19 @@ findProjects ::
   OsInfo ->
   Path Abs Dir ->
   m [DpkgDatabase]
-findProjects osInfo = walkWithFilters' $ \dir _ files -> do
-  case findFileNamed "status" files of
-    Nothing -> pure ([], WalkContinue)
-    Just file -> do
-      if (Text.isInfixOf "var/lib/dpkg/" $ toText . toFilePath $ file)
-        then pure ([DpkgDatabase dir file osInfo], WalkContinue)
-        else pure ([], WalkContinue)
+findProjects osInfo = walkWithFilters' $ \dir dirs files -> do
+  let standardDBs = case findFileNamed "status" files of
+        Just file -> 
+          if Text.isInfixOf "var/lib/dpkg/" (toText . toFilePath $ file)
+            then [DpkgDatabase dir file osInfo]
+            else []
+        Nothing -> []
+  statusD_DBs <- case find (\f -> toFilePath f == "var/lib/dpkg/status.d/") dirs of
+      Just dir' -> do
+        (_, filesInDir) <- listDir dir'
+        pure $ map (\file -> DpkgDatabase dir' file osInfo) (filter (not . Text.isSuffixOf ".md5sums" . toText) filesInDir)
+      Nothing -> pure []
+  pure (standardDBs ++ statusD_DBs, WalkContinue)
 
 mkProject :: DpkgDatabase -> DiscoveredProject DpkgDatabase
 mkProject project =
