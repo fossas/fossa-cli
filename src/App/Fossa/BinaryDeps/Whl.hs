3cfe2e8ccabbca3a7d9e39b746488e5ca0b77dc7
Gaurav Bhagchandani <gaurav@fossa.com>
2026-08-24 16:50:50 -0700

    [ANE-1349] Binary discovery support for whl files (#1750)
    * First attempt at wheel file parsing and enumeration
    
    * License detection working, but dep needs to be renamed
    
    * Change return type of binary scanning to allow for user defined deps and normal source units
    
    * Add conversion of user defined deps to locator deps
    
    * Allow default value if license is missing
    
    * Add logic to get old license key if new license key does not exist
    
    * Remove logging
    
    * Add tests for whl files
    
    * Format
    
    * Update tests
    
    * Update docs
    
    * Update changelog
    
    * Switch to checking suffix instead of infix

---
+++ src/App/Fossa/BinaryDeps/Whl.hs
@@ -0,0 +1,120 @@
+-- Reference: https://packaging.python.org/en/latest/specifications/core-metadata/
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module App.Fossa.BinaryDeps.Whl (resolveWhl) where
+
+import Control.Algebra (Has)
+import Control.Carrier.Diagnostics (
+  Diagnostics,
+  ToDiagnostic (renderDiagnostic),
+  context,
+  errCtx,
+  fromMaybeText,
+  recover,
+  warnOnErr,
+ )
+import Control.Carrier.Finally (runFinally)
+import Control.Effect.Lift (Lift)
+import Control.Monad (join)
+import Data.List (find, isSuffixOf)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Maybe (fromMaybe)
+import Data.String.Conversion (ToString (toString), ToText (toText))
+import Data.Text (Text)
+import Data.Text qualified as Text
+import DepTypes (DepType (PipType))
+import Discovery.Archive (extractZip, withArchive)
+import Effect.Logger (Logger, logDebug, pretty, viaShow)
+import Effect.ReadFS (ReadFS, listDir, readContentsText)
+import Errata (Errata (..))
+import Path (Abs, Dir, File, Path, dirname, filename, mkRelFile, (</>))
+import Path.Extra (renderRelative, tryMakeRelative)
+import Srclib.Types (BinaryDiscoveredDep (..), SourceUserDefDep (..))
+import System.FilePath (dropTrailingPathSeparator)
+
+data WhlMetadata = WhlMetadata
+  { whlName :: Text
+  , whlVersion :: Text
+  , whlLicense :: Text
+  }
+
+-- | Implement .whl resolution using a similar method to Ant analysis in CLIv1.
+-- The overall idea is to:
+--   1. Extract the whl to a temporary directory (it's a zip!)
+--   2. Search inside for a file named `*.dist-info/METADATA` parse it and return metadata derived from it.
+resolveWhl :: (Has (Lift IO) sig m, Has Diagnostics sig m, Has Logger sig m, Has ReadFS sig m) => Path Abs Dir -> Path Abs File -> m (Maybe BinaryDiscoveredDep)
+resolveWhl _ file | not $ fileHasSuffix file [".whl"] = pure Nothing
+resolveWhl root file = do
+  let fileDescription = toText file
+  logDebug $ "Inferring metadata from " <> pretty fileDescription
+  result <- recover
+    . warnOnErr (FailedToResolveWhl file)
+    . errCtx (FailedToResolveWhlCtx file)
+    . context ("Infer metadata from " <> fileDescription)
+    . runFinally
+    $ withArchive extractZip file
+    $ \dir -> tacticMetadata dir
+  pure $ fmap (toBinaryDiscoveredDep root file) (join result)
+
+newtype FailedToResolveWhl = FailedToResolveWhl (Path Abs File)
+
+instance ToDiagnostic FailedToResolveWhl where
+  renderDiagnostic (FailedToResolveWhl path) = do
+    let header = "Could not infer whl metadata (license, whl name, and version) from " <> toText path
+    Errata (Just header) [] Nothing
+
+newtype FailedToResolveWhlCtx = FailedToResolveWhlCtx (Path Abs File)
+
+instance ToDiagnostic FailedToResolveWhlCtx where
+  renderDiagnostic (FailedToResolveWhlCtx path) = do
+    let header = "Ensure " <> toText path <> " is a valid whl file"
+    Errata (Just header) [] Nothing
+
+tacticMetadata :: (Has (Lift IO) sig m, Has Diagnostics sig m, Has Logger sig m, Has ReadFS sig m) => Path Abs Dir -> m WhlMetadata
+tacticMetadata archive = context ("Parse metadata for " <> toText archive) $ do
+  distInfoDir <- findDistInfoFolder archive
+  let metadataPath = distInfoDir </> $(mkRelFile "METADATA")
+  do
+    content <- readContentsText metadataPath
+    logDebug $ "Parsing METADATA file: " <> pretty (renderRelative archive metadataPath)
+    metadataToWhlMeta $ parseMetadata content
+
+findDistInfoFolder :: (Has ReadFS sig m, Has Diagnostics sig m, Has Logger sig m) => Path Abs Dir -> m (Path Abs Dir)
+findDistInfoFolder archive = do
+  (dirs, _) <- listDir archive
+  logDebug $ "Listing folders in archive " <> viaShow (map (dropTrailingPathSeparator . toString . dirname) dirs)
+  logDebug $ "Listing folder matching in archive " <> viaShow (map (\d -> dirHasSuffix d [".dist-info"]) dirs)
+  fromMaybeText "Could not find *.dist-info folder in whl archive" $
+    find (\d -> dirHasSuffix d [".dist-info"]) dirs
+
+parseMetadata :: Text -> Map Text Text
+parseMetadata t = Map.fromList . map strip' . filter' $ map (Text.breakOn ":") (Text.lines t)
+  where
+    null' (a, b) = any Text.null [a, b]
+    strip' (a, b) = (Text.strip a, Text.strip $ Text.drop 1 b)
+    filter' = filter (not . null')
+
+metadataToWhlMeta :: (Has Diagnostics sig m) => Map Text Text -> m WhlMetadata
+metadataToWhlMeta metadata =
+  WhlMetadata
+    <$> fromMaybeText "Missing whl name" (Map.lookup "Name" metadata)
+    <*> fromMaybeText "Missing whl version" (Map.lookup "Version" metadata)
+    <*> fromMaybeText "Missing whl license" (Just (getLicenseFromMetadata metadata))
+
+getLicenseFromMetadata :: Map Text Text -> Text
+getLicenseFromMetadata metadata = do
+  let oldLicense = fromMaybe "" (Map.lookup "License" metadata)
+  fromMaybe oldLicense (Map.lookup "License-Expression" metadata)
+
+fileHasSuffix :: Path a File -> [String] -> Bool
+fileHasSuffix file = any (\suffix -> suffix `isSuffixOf` toString (filename file))
+
+dirHasSuffix :: Path a Dir -> [String] -> Bool
+dirHasSuffix dir = any (\suffix -> suffix `isSuffixOf` dropTrailingPathSeparator (toString (dirname dir)))
+
+toBinaryDiscoveredDep :: Path Abs Dir -> Path Abs File -> WhlMetadata -> BinaryDiscoveredDep
+toBinaryDiscoveredDep root file WhlMetadata{..} = do
+  let rel = tryMakeRelative root file
+  LocatorDep (PipType, SourceUserDefDep whlName whlVersion whlLicense (Just whlName) Nothing (Just rel))
