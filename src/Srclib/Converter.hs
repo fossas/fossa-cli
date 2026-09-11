1d8a95a6fa5e984c74b808abee09c221a1b99922
Gaurav Bhagchandani <gaurav@fossa.com>
2026-09-11 02:23:32 -0700

    Fix missing ConanType references

--- src/Srclib/Converter.hs
+++ src/Srclib/Converter.hs
@@ -188,6 +188,7 @@ fetcherToDepType fetcher | depTypeToFetcher BowerType == fetcher = Just BowerTyp
 fetcherToDepType fetcher | depTypeToFetcher CarthageType == fetcher = Just CarthageType
 fetcherToDepType fetcher | depTypeToFetcher CargoType == fetcher = Just CargoType
 fetcherToDepType fetcher | depTypeToFetcher ComposerType == fetcher = Just ComposerType
+fetcherToDepType fetcher | depTypeToFetcher ConanType == fetcher = Just ConanType
 fetcherToDepType fetcher | depTypeToFetcher CondaType == fetcher = Just CondaType
 fetcherToDepType fetcher | depTypeToFetcher CpanType == fetcher = Just CpanType
 fetcherToDepType fetcher | depTypeToFetcher CranType == fetcher = Just CranType
